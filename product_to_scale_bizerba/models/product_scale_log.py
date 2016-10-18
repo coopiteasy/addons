97412b49a2b78895955f7a5435116e8de243e28a
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-10-18 11:48:32 +0200

    [FIX] flake8

--- product_to_scale_bizerba/models/product_scale_log.py
+++ product_to_scale_bizerba/models/product_scale_log.py
@@ -3,8 +3,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import logging
-
+# import logging
 from openerp import tools
 from openerp.osv import fields
 from openerp.osv.orm import Model
