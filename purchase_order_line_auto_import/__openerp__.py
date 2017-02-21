af3e1d2c0820dc4ec1cc679f3358ef40a14d4740
houssine <houssine.bakkali@gmail.com>
2017-02-21 18:27:33 +0100

    [ADD] add modules on the repos

---
+++ purchase_order_line_auto_import/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2016 Houssine BAKKALI, Open Architects Consulting SPRL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Purchase order line auto import",
+    'summary': """
+        This module allows to create automatically line with the product and minimal quantities
+        when selecting the partner. The user can then delete the line that he doesn't want and
+        update the quantity for each product on the line.
+    """,
+    'author': 'Houssine BAKKALI, Open Architects Consulting SPRL',
+    'category': 'Purchase Management',
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': ['purchase',],
+    'data': [],
+}
