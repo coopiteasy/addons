d308566b5783ab336a5dffdeea999a57aca63ecd
houssine <houssine.bakkali@gmail.com>
2017-04-27 12:13:41 +0200

    [ADD] add module extending the product_to_scale_bizerba. here fixing a limitation that doesn't display the field on the product_template view. It allows you to not activate the product variant on Odoo.

---
+++ product_to_scale_bizerba_extended/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2017 Houssine BAKKALI, Coop IT Easy
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Product to scale bizerba extended",
+    'summary': """
+        This module gather the extension to the module product_to_scale_bizerba.
+    """,
+    'author': 'Houssine BAKKALI, Coop IT Easy',
+    'category': 'Sales',
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': ['product_to_scale_bizerba'],
+    'data': [
+        'views/product_view.xml',         
+    ],
+}
