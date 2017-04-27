9affec31ef17c1ff5cd6705884ea5ad77208ab56
houssine <houssine.bakkali@gmail.com>
2017-04-27 12:11:16 +0200

    [TYPO] fix typo

--- product_internal_ref/models/product.py
+++ product_internal_ref/models/product.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2017 Coop IT Easy (http://www.coopiteasy.be)
+# © 2017 Houssine BAKKALI - Coop IT Easy 
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import fields, models
