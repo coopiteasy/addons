da0351da0744f672fcb2ccbc2e5df46b5282e8d0
houssine <houssine.bakkali@gmail.com>
2017-04-27 12:14:24 +0200

    [TYPO] fix typo

--- purchase_order_line_auto_import/models/purchase.py
+++ purchase_order_line_auto_import/models/purchase.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# � 2016 Houssine BAKKALI, Open Architects Consulting SPRL
+# © 2016 Houssine BAKKALI, Open Architects Consulting SPRL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
