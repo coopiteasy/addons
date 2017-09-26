5db8c62ff366c569498a01b4602d7cb77cc437fb
houssine <houssine.bakkali@gmail.com>
2017-09-26 18:43:00 +0200

    [LIC] fix licence

--- purchase_order_line_auto_import/__openerp__.py
+++ purchase_order_line_auto_import/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Houssine BAKKALI, Open Architects Consulting SPRL
+# © 2016 Houssine BAKKALI, Coop IT Easy SCRL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Purchase order line auto import",
@@ -8,7 +8,7 @@
         when selecting the partner. The user can then delete the line that he doesn't want and
         update the quantity for each product on the line.
     """,
-    'author': 'Houssine BAKKALI, Open Architects Consulting SPRL',
+    'author': 'Houssine BAKKALI, Coop IT Easy SCRL',
     'category': 'Purchase Management',
     'version': '9.0.1.0.0',
     'license': 'AGPL-3',
