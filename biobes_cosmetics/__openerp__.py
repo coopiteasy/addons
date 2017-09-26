79f22b85c55d4728b5667f7075b155dda9769510
houssine <houssine.bakkali@gmail.com>
2017-09-26 14:10:06 +0200

    [ADD] add module to gather cosmetics change for biobes.

---
+++ biobes_cosmetics/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2017 Houssine BAKKALI, Coop IT Easy SCRL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Biobes Cosmetics",
+    'summary': """
+        This module brings just some cosmetics changes.
+    """,
+    'author': 'Houssine BAKKALI, Coop IT Easy SCRL',
+    'category': 'Purchase Management',
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': ['product','beesdoo_product',],
+    'data': [
+        'views/product_view.xml',
+    ],
+}
+
