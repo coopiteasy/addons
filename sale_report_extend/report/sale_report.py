5736dff2186e09b90d386c2e9b8c42824ed7158d
houssine <houssine.bakkali@gmail.com>
2017-11-20 11:27:19 +0100

    [ADD] add the possibility to group the sales report on the partner category (tags)

---
+++ sale_report_extend/report/sale_report.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from openerp import api, fields, models, _
+
+class SaleReport(models.Model):
+    _inherit = "sale.report"
+    
+    category_id = fields.Many2one('res.partner.category', string='Partner Category', readonly=True)
+
+    def _select(self):
+        return  super(SaleReport, self)._select() + ", pcr.category_id as category_id"
+    
+    def _from(self):
+        return  super(SaleReport, self)._from() + " join res_partner_res_partner_category_rel pcr on (pcr.partner_id=s.partner_id)"
+            
+    def _group_by(self):
+        return  super(SaleReport, self)._group_by() + ", pcr.category_id"
+            
