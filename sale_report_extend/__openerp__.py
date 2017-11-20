f63c7f60b9db53e7b269403b4c8841bde118f7cb
houssine <houssine.bakkali@gmail.com>
2017-11-20 11:27:19 +0100

    [ADD] add the possibility to group the sales report on the partner category (tags)

---
+++ sale_report_extend/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 Houssine BAKKALI, Coop IT Easy SCRL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Sale Report Extension",
+    'summary': """
+        This module extends the sale report to extra fields.
+    """,
+    'author': 'Houssine BAKKALI, Coop IT Easy SCRL',
+    'category': 'Sales',
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': ['sale',],
+    'data': [
+        'report/sale_report.xml',
+    ],
+}
+
