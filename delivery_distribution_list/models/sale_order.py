549f6586d88c036db50357f4703ba84d6c7fa818
houssine <houssine.bakkali@gmail.com>
2018-01-04 15:48:59 +0100

    [FIX] change name of the field carrier_id on sale_order and stock_picking to solve incompatibility with standard delivery module

--- delivery_distribution_list/models/sale_order.py
+++ delivery_distribution_list/models/sale_order.py
@@ -5,13 +5,13 @@ from openerp import api, fields, models, _
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
-    carrier_id = fields.Many2one('res.partner', string="Assigned carrier", readonly=True)
+    distribution_carrier_id = fields.Many2one('res.partner', string="Assigned carrier", readonly=True)
     distribution_list_id = fields.Many2one('delivery.distribution.list', string="Distribution list", readonly=True)
     
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
     
-    carrier_id = fields.Many2one(comodel_name='res.partner', compute='_compute_distribution_fields', string="Assigned carrier", store=True)
+    distribution_carrier_id = fields.Many2one(comodel_name='res.partner', compute='_compute_distribution_fields', string="Assigned carrier", store=True)
     distribution_list_id = fields.Many2one(comodel_name='delivery.distribution.list', compute='_compute_distribution_fields', string="Distribution list", store=True)
     
     @api.multi
