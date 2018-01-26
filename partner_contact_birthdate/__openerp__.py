0b30bb0675f93af2cd94b5d543a49f44c762a2b0
houssine <houssine.bakkali@gmail.com>
2018-01-26 16:34:42 +0100

    [ADD] add oca modules to implement birthdate on res_partner

---
+++ partner_contact_birthdate/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-2015  Grupo ESOC <www.grupoesoc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Contact's birthdate",
+    "version": "9.0.1.0.0",
+    "author": "Jairo Llopis,"
+              "Odoo Community Association (OCA)",
+    "category": "Customer Relationship Management",
+    "website": "https://odoo-community.org/",
+    "depends": [
+        "partner_contact_personal_information_page",
+    ],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    'installable': True,
+}
