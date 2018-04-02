9b2f6ae7c7fb6ed8688aca73cf8570097141672a
houssine <houssine.bakkali@gmail.com>
2018-04-02 20:31:51 +0200

    [ADD] add module adding vehicule info on resource

---
+++ resource_vehicule/models/resource_vehicule.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Coop IT Easy SCRLfs.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import _, api, fields, models
+
+
+class ResourceCategory(models.Model):
+    _inherit = 'resource.category'
+
+    vehicule = fields.Boolean(string="Vehicule")
+    vehicule_type = fields.Selection([('bike','Bike'),
+                                      ('car','Car')],string="Vehicule type")
+
+class ResourceResource(models.Model):
+    _inherit = 'resource.resource'
+    
+    id_number = fields.Char(string="ID number")
+    chassis_number = fields.Char(string="Chassis number", copy=False)
+    brand_id = fields.Many2one('resource.brand', string="Brand")
+    color = fields.Many2one('resource.color', string="Color")
+    model_id = fields.Many2one('resource.model', string="Model")
+    gearbox = fields.Many2one('resource.gearbox', string="Gearbox")
+    vehicule_type = fields.Selection(related='category_id.vehicule_type')
+
+class ResourceBrand(models.Model):
+    _name = 'resource.brand'
+    
+    name = fields.Char(string="Brand")
+    code = fields.Char(string="Code")
+
+class ResourceModel(models.Model):
+    _name = 'resource.model'
+    
+    name = fields.Char(string="Model")
+    code = fields.Char(string="Code")
+
+class ResourceColor(models.Model):
+    _name = 'resource.color'
+    
+    name = fields.Char(string="Color")
+    code = fields.Char(string="code")
+    
+class ResourceGearbox(models.Model):
+    _name = 'resource.gearbox'
+    
+    name = fields.Char(string="Gearbox")
+    code = fields.Char(string="code")
