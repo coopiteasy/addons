0a6a83eedfb3b0e1c186fa0d8357b1bccd698a8e
houssine <houssine@DESKTOP-ICALFFT.lan>
2018-05-23 12:19:26 +0200

    [IMP] change the field relation type to allow to have the same product in different categories

--- resource_activity/models/resource.py
+++ resource_activity/models/resource.py
@@ -8,9 +8,9 @@ from openerp.exceptions import ValidationError, UserError
 class ResourceCategory(models.Model):
     _inherit = 'resource.category'
     
-    product_ids = fields.One2many('product.product', 'resource_category_id', string="Product")
+    product_ids = fields.Many2many('product.product', string="Product")
     
 class ProductProduct(models.Model):
     _inherit = 'product.product'
     
-    resource_category_id = fields.Many2one('resource.category', string="Resource category")
+    resource_category_id = fields.Many2many('resource.category', string="Resource category")
