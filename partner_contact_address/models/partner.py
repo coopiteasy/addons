05d6057c6f94116ecd30185e94e12450723bc7b1
houssine <houssine@192.168.12.70>
2018-06-18 16:05:36 +0200

    [ADD] module to add the possibility to have a representative with his own address
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ partner_contact_address/models/partner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Coop IT Easy SCRLfs.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    type = fields.Selection(selection_add=[('representative', 'Representative')])
+
+
+
