ae23305473fac6cebdef91050a969b6cf545b078
Rémy Taymans <remytaymans@gmail.com>
2018-07-02 15:00:13 +0200

    [ADD] Module partner_communication_lang
    Let add a communication language to partners.

---
+++ partner_communication_lang/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 Rémy Taymans <remytaymans@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Partner Communication Language',
+
+    'summary': """
+    Let you add configurable languages to contacts.
+    """,
+    'description': """
+    """,
+
+    'author': 'Rémy Taymans',
+    'license': 'AGPL-3',
+    'version': '9.0.1.0',
+    'website': "https://github.com/houssine78/addons",
+
+    'category': '',
+
+    'depends': [
+        'base',
+    ],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/partner.xml',
+        'views/partner_communication_lang.xml',
+    ]
+}
