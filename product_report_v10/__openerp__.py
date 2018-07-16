937d7c46a88b1797055297601559b5ddfa820e69
houssine <houssine@192.168.12.70>
2018-07-16 11:16:03 +0200

    [ADD] new module to print product bare code on a format 8x3. Two report are possible repeat 24 the same product or print 24 products on one page.

---
+++ product_report_v10/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 Houssine BAKKALI, Open Architects Consulting SPRL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Barcode product report",
+    'summary': """
+        This module allows to show the print barcode and name of the product.
+    """,
+    'author': 'Houssine BAKKALI, Coop IT Easy',
+    'category': 'Product',
+    'version': '9.0.1.0.0',
+    'website': 'www.coopiteasy.be',
+    'license': 'AGPL-3',
+    'depends': ['base','product','report'],
+    'data': [
+        'report/product_template_label_repeat.xml',
+        'report/product_template_templates.xml',
+        'report/product_reports.xml',         
+    ],
+}
