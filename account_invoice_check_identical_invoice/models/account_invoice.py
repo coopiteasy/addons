54bbbc1164e2c99cdabb25dd5ca5ac0fceb0647c
robinkeunen <robin@keunen.net>
2018-07-23 17:38:23 +0200

    add onchange function

--- account_invoice_check_identical_invoice/models/account_invoice.py
+++ account_invoice_check_identical_invoice/models/account_invoice.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api, _
 from openerp.exceptions import ValidationError
+import datetime as dt
 
 
 class AccountInvoice(models.Model):
@@ -8,9 +9,16 @@ class AccountInvoice(models.Model):
 
     identical_invoice_confirmed = fields.Boolean('Confirm Identical Invoice?',
                                                  default=False,
+                                                 copy=False,
                                                  help="HELP!!!")
     identical_invoice_detected = fields.Boolean('identical_invoice_detected',
                                                 compute='_check_identical_invoice')
+    # todo onchange
+
+    @api.onchange('partner_id', 'date_invoice')
+    @api.multi
+    def _onchange_check_identical_invoice(self):
+        self._check_identical_invoice()
 
     @api.multi
     def _check_identical_invoice(self):
@@ -19,11 +27,20 @@ class AccountInvoice(models.Model):
             duplicate_domain = [
                 ('state', 'not in', ['draft', 'cancel']),
                 ('partner_id', '=', invoice.partner_id.id),
-                ('date_invoice', '=', invoice.date_invoice),
             ]
-            duplicate_invoices = Invoice.search(duplicate_domain)
-            duplicate_invoices = duplicate_invoices.filtered(
-                lambda i: round(i.amount_total, 2) == round(invoice.amount_total, 2)
+            partner_invoices = Invoice.search(duplicate_domain)
+
+            def equal_amount(i):
+                return round(i.amount_total, 2) == round(invoice.amount_total, 2)
+
+            def same_date(i):
+                return i.date_invoice == invoice.date_invoice
+
+            def invoiced_today(i):
+                return i.date_invoice == str(dt.date.today()) and not invoice.date_invoice
+
+            duplicate_invoices = partner_invoices.filtered(
+                lambda i: equal_amount(i) and (same_date(i) or invoiced_today(i))
             )
 
             if duplicate_invoices:
@@ -31,13 +48,13 @@ class AccountInvoice(models.Model):
             else:
                 self.identical_invoice_detected = False
 
-
     @api.multi
     def invoice_validate(self):
+        self._check_identical_invoice()
         for invoice in self:
             if invoice.identical_invoice_detected and not invoice.identical_invoice_confirmed:
                 raise ValidationError('We detected invoices with the same partner, date'
-                                      ' and total. Please check the' 
-                                      'Confirm Identical Invoice? box to continue')
+                                      ' and total. \n\nPlease check the' 
+                                      ' "Confirm Identical Invoice?" box to continue')
 
         return super(AccountInvoice, self).invoice_validate()
