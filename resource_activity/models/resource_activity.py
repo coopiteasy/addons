c6f18fe8bd645ec42eac66ca3254db86d7053e69
robinkeunen <robin@keunen.net>
2018-08-07 10:37:38 +0200

    [IMP] avoid unecessary calls to backend

--- resource_activity/models/resource_activity.py
+++ resource_activity/models/resource_activity.py
@@ -52,11 +52,7 @@ class ResourceActivity(models.Model):
     _inherit = ['mail.thread']
 
     @api.multi
-    @api.depends('registrations.need_push',
-                 'participation_product_id',
-                 'delivery_product_id',
-                 'guides', 'guide_product_id',
-                 'need_guide')
+    @api.depends('registrations.need_push')
     def _compute_push2sale_order(self):
         # computed field in order to display or not the push to sale order
         # button
@@ -65,7 +61,8 @@ class ResourceActivity(models.Model):
                 registrations_need_push = any(
                     activity
                     .registrations
-                    .filtered(lambda record: record.state in ['option', 'booked'])
+                    .filtered(
+                        lambda record: record.state in ['option', 'booked'])
                     .mapped('need_push'))
                 activity.need_push = activity.need_push or registrations_need_push
         return
@@ -660,28 +657,31 @@ class ResourceActivity(models.Model):
     def write(self, vals):
         for activity in self:
             if activity.sale_orders:
-                if 'need_delivery' in vals:
-                    if not vals.get('need_delivery'):
-                        vals['delivery_place'] = ''
-                        vals['delivery_time'] = False
-                        vals['pickup_place'] = ''
-                        vals['pickup_time'] = False
-                        vals['delivery_product_id'] = False
-                    vals['need_push'] = True
-                if 'need_guide' in vals:
-                    if not vals.get('need_guide'):
-                        vals['guide_product_id'] = False
-                        vals['guides'] = [[6, False, []]]
-                    vals['need_push'] = True
-                if 'guide_product_id' in vals:
-                    vals['need_push'] = True
-                if 'guides' in vals:
-                    vals['need_push'] = True
-                if 'need_participation' in vals:
-                    if not vals.get('need_participation'):
-                        vals['need_participation'] = False
-                    vals['need_push'] = True
-                if 'activity_type' in vals:
+                if ('need_delivery' in vals
+                        and not vals.get('need_delivery')):
+
+                    vals['delivery_place'] = ''
+                    vals['delivery_time'] = False
+                    vals['pickup_place'] = ''
+                    vals['pickup_time'] = False
+                    vals['delivery_product_id'] = False
+
+                if 'need_guide' in vals and not vals.get('need_guide'):
+                    vals['guide_product_id'] = False
+                    vals['guides'] = [[6, False, []]]
+
+                if ('need_participation' in vals
+                        and not vals.get('need_participation')):
+
+                    vals['need_participation'] = False
+
+                watches = (
+                    'need_delivery', 'delivery_product_id',
+                    'need_guide', 'guide_product_id', 'guides',
+                    'need_participation', 'participation_product_id',
+                    'activity_type',
+                )
+                if any(map(lambda var: var in vals, watches)):
                     vals['need_push'] = True
         return super(ResourceActivity, self).write(vals)
 
@@ -689,8 +689,7 @@ class ResourceActivity(models.Model):
     @api.depends('partner_id', 'registrations_max', 'registrations_expected')
     def _propagate_activity_fields_update(self):
         for activity in self:
-            vals = {}
-            vals['partner_id'] = activity.partner_id
-            vals['registrations_max'] = activity.registrations_max
-            vals['registrations_expected'] = activity.registrations_expected
+            vals = {'partner_id': activity.partner_id,
+                    'registrations_max': activity.registrations_max,
+                    'registrations_expected': activity.registrations_expected}
             activity.registrations.write(vals)
