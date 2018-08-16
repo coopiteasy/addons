382838631011a2f4f317a0cf954d2d66c77e11e9
robinkeunen <robin@keunen.net>
2018-08-16 17:04:59 +0200

    round cash line due amount and precomputed line amount

---
+++ pos_round_cash_payment_line/static/src/js/pos_round_cash_payment_line.js
@@ -0,0 +1,72 @@
+/*
+    POS Round cash Payment line for odoo
+    Copyright (C) 2018 Robin Keunen
+    @author: Robin Keunen
+    The licence is in the file __openerp__.py
+*/
+
+odoo.define('pos_round_cash_payment_line.pos_round_cash_payment_line', function (require) {
+    "use strict";
+    var models = require('point_of_sale.models');
+    var utils = require('web.utils');
+
+    models.Order = models.Order.extend({
+
+        round_5c: function(x) {
+            return utils.round_precision(x * 20) / 20;
+        },
+
+        round_5c_remainder: function(x) {
+            var rounded_value = utils.round_precision(x * 20) / 20;
+            return x - rounded_value;
+        },
+
+        get_due: function(paymentline) {
+            let due;
+            if (!paymentline) {
+                due = this.get_total_with_tax() - this.get_total_paid();
+                alert('get due !paymentline');
+                return utils.round_precision(Math.max(0,due), this.pos.currency.rounding);
+            } else {
+                due = this.get_total_with_tax();
+                var lines = this.paymentlines.models;
+                for (var i = 0; i < lines.length; i++) {
+                    if (lines[i] === paymentline) {
+                        break;
+                    } else {
+                        due -= lines[i].get_amount();
+                    }
+                }
+
+                if (paymentline.cashregister.journal.type === 'cash') {
+                    alert('get due round_c5');
+                    return this.round_5c(due);
+                } else {
+                    alert('get due else');
+                    return utils.round_precision(Math.max(0, due), this.pos.currency.rounding);
+                }
+            }
+        },
+
+
+        add_paymentline: function(cashregister) {
+            this.assert_editable();
+            var newPaymentline = new models.Paymentline(
+                {},
+                {
+                    order: this,
+                    cashregister:cashregister, pos: this.pos
+                }
+            );
+            if(cashregister.journal.type !== 'cash'
+                || this.pos.config.iface_precompute_cash) {
+                const due = this.get_due(newPaymentline);
+                newPaymentline.set_amount( due );
+            } else {
+                newPaymentline.set_amount( 0 );
+            }
+            this.paymentlines.add(newPaymentline);
+            this.select_paymentline(newPaymentline);
+        },
+    });
+});
