51a77aba3c63bff58fa205870b585791d61d768f
Rémy Taymans <remytaymans@gmail.com>
2018-10-19 14:46:16 +0200

    [FIX] p_t_scale_bizerba_extended: PEP8 models

--- product_to_scale_bizerba_extended/models/product_scale_group.py
+++ product_to_scale_bizerba_extended/models/product_scale_group.py
@@ -2,26 +2,33 @@
 # © 2017 Coop IT Easy (http://www.coopiteasy.be)
 # @author: Houssine BAKKALI (https://github.com/houssine78)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from datetime import datetime
-
 from openerp import api, fields, models, _
 import openerp.addons.decimal_precision as dp
 
+
 class product_scale_group(models.Model):
     _inherit = 'product.scale.group'
-    
-    product_ids = fields.One2many('product.template', 'scale_group_id', string='Products')
-    
+
+    product_ids = fields.One2many(
+        'product.template',
+        'scale_group_id',
+        string='Products'
+    )
+
     @api.multi
     def send_all_to_scale_create(self):
         for scale_group in self:
-            scale_group.product_ids.filtered(lambda r: r.to_weight == True and r.sale_ok == True).send_scale_create()
-            
+            (scale_group.product_ids
+             .filtered(lambda r: r.to_weight and r.sale_ok)
+             .send_scale_create())
+
     @api.multi
     def send_all_to_scale_write(self):
         for scale_group in self:
-            scale_group.product_ids.filtered(lambda r: r.to_weight == True and r.sale_ok == True).send_scale_write()
-    
+            (scale_group.product_ids
+             .filtered(lambda r: r.to_weight and r.sale_ok)
+             .send_scale_write())
+
     @api.multi
     def send_all_to_scale_unlink(self):
         for scale_group in self:
