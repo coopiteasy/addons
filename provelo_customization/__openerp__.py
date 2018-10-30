0bac14faad7ca63eb772149681bbb7eaed8eb2d3
robinkeunen <robin@keunen.net>
2018-10-30 16:42:36 +0100

    [ADD] location filters

---
+++ provelo_customization/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2017- Coop IT Easy.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Provelo Customization",
+    "version": "9.0.1.0",
+    "depends": [
+        'resource_activity',
+    ],
+    "author": "Coop IT Easy - Robin Keunen <robin@coopiteasy.be>",
+    "license": "AGPL-3",
+    "category": "",
+    "website": "www.coopiteasy.be",
+    "description": """
+        Specifics customizations for Pro Velo
+    """,
+    'data': [
+        'views/location_filters.xml'
+    ],
+    'installable': True,
+}
