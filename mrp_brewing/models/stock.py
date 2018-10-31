6746ffa8da14423080a361d28d96af28748df0f3
robinkeunen <robin@keunen.net>
2018-10-31 15:30:06 +0100

    [IMP] order production lots by has_stock desc, create_date asc

--- mrp_brewing/models/stock.py
+++ mrp_brewing/models/stock.py
@@ -35,28 +35,33 @@ class StockWarehouseOrderpoint(models.Model):
 
 class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
+    _order = 'has_stock desc, create_date asc'
 
     qty_available = fields.Float(
-        compute="_compute_qty_available",
         string='Quantity available',
+        compute="_compute_qty_available",
         store=True)
 
-    def _compute_lot_quantity(self, lot):
-        quants = lot.quant_ids.filtered(
-            lambda r: r.location_id.usage == 'internal' and not r.reservation_id)
-        lot.qty_available = sum(quants.mapped('qty'))
+    has_stock = fields.Boolean(
+        string='Has Stock',
+        compute="_compute_qty_available",
+        store=True,
+    )
 
     @api.multi
     @api.depends('quant_ids.reservation_id')
     def _compute_qty_available(self):
         for lot in self:
-            self._compute_lot_quantity(lot)
+            quants = lot.quant_ids.filtered(
+                lambda r: r.location_id.usage == 'internal' and not r.reservation_id)  # noqa
+            qty = sum(quants.mapped('qty'))
+            lot.qty_available = qty
+            lot.has_stock = qty > 0
 
     @api.model
     def _batch_compute_qty_available(self):
         lots = self.env['stock.production.lot'].search([])
-        for lot in lots:
-            self._compute_lot_quantity(lot)
+        lots._compute_qty_available()
 
 
 class StockPicking(models.Model):
