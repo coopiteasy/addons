51b1bce34506ef392325d09a65b102e2a8803a52
houssine <houssine@DESKTOP-ICALFFT>
2018-11-14 11:37:25 +0100

    [ADD] new module that add the delivery address on invoice template

---
+++ medor_delivery_address/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2017- Coop IT Easy.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Medor Delivery address",
+    "version": "1.0",
+    "depends": ["medor_sexy_invoice",
+                "sale"],
+    "author": "Houssine BAKKALI <houssine@coopiteasy.be>",
+    "category": "Invoice",
+    "description": """
+    This module add the delivery address on the invoice template.
+    """,
+    'data': [
+        'report/docs_report.xml',
+        'report/sexy_invoice.xml',
+    ],
+    'installable': True,
+    "auto_install": True,
+}
