6bd8f0e56542eed16f7a356f612a7f2c82a33923
houssine <houssine@DESKTOP-ICALFFT>
2018-12-10 15:24:51 +0100

    [ADD] add module implementing BBA communication on sale order

---
+++ l10n_be_sale_order_bba/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2017 Coop IT Easy (http://www.coopiteasy.be)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Order BBA',
+    'category': 'Sales',
+    'author': "Coop IT Easy - Houssine BAKKALI <houssine@coopiteasy.be>",
+    'website': 'www.coopiteasy.be',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+        'l10n_be_invoice_bba'
+    ],
+    "description": """
+    This module implements the bba structured communication on the sale order.
+    """,
+    'data': [
+        "views/sale_views.xml",
+    ],
+    'installable': True,
+}
