4b92a1b1841d72ea478f8fdf6b4007a9835a8827
houssine <houssine@DESKTOP-ICALFFT>
2019-01-03 17:12:29 +0100

    [TYPO] put right category for the module

--- account_invoice_reminder/__openerp__.py
+++ account_invoice_reminder/__openerp__.py
@@ -23,7 +23,7 @@
     "version": "1.0",
     "depends": ["account"],
     "author": "Houssine BAKKALI <houssine.bakkali@gmail.com>",
-    "category": "CRM",
+    "category": "Accounting",
     "description": """
     This module just adds two fields to say when we sent a payment reminder to the 
     customer and the level of the reminder.    
