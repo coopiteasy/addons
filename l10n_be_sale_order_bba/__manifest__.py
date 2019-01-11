c1bf4b27cca47e8caae3bedb0b813a058c373999
houssine <houssine@DESKTOP-ICALFFT>
2019-01-11 17:19:57 +0100

    [FIX] fix encoding

--- l10n_be_sale_order_bba/__manifest__.py
+++ l10n_be_sale_order_bba/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2017 Coop IT Easy (http://www.coopiteasy.be)
+# Copyright 2018 - ongoing Coop IT Easy (http://www.coopiteasy.be)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
