b5d16025a2dbf97ff361a60fee5f8f823822ca7f
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-01-14 16:50:14 +0100

    Add init and openerp files

---
+++ remaining_time_project_on_task/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2017- Coop IT Easy.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Remaining time project on task",
+    "version": "1.0",
+    "depends": [
+        'base',
+    ],
+    "author": "Coop IT Easy - Nicolas Jamoulle <nciolas@coopiteasy.be>",
+    "license": "AGPL-3",
+    "category": "",
+    "website": "www.coopiteasy.be",
+    "description": """
+        Show remaining time project on task view
+    """,
+    "data": [
+        'views/purchaseorder.xml',
+    ],
+    'installable': True,
+}
