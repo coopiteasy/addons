e5c5cf172ff752259c483c9b14bb39e8e3b8a929
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-01-19 16:27:26 +0100

    Add test funtion _compute_remaining_hours_contract

--- project_remaining_time_on_task/models/project.py
+++ project_remaining_time_on_task/models/project.py
@@ -4,4 +4,12 @@ from openerp import api, fields, models
 
 class Task(models.Model):
     _inherit = 'project.task'
-    remaining_hours_contract = fields.Float('Heures restantesc/ contrat', digits=(16,2), help="Total remaining time from the contract")
+
+    # fields.function(_compute_remaining_hours_contract,'Heures restantes / contrat', digits=(16, 2),
+    #                                     help="Total remaining time from the contract")
+
+    def _compute_remaining_hours_contract(self):
+        return 2.0
+
+    remaining_hours_contract = fields.function(_compute_remaining_hours_contract, 'Heures restantes / contrat',
+                                               type='digits=(16, 2)')
