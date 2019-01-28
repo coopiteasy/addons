08f78dd6a9ed4618ce56b11db81713a9fc7e422e
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-01-28 14:23:54 +0100

    corrections pull request

--- project_remaining_time_on_task/models/project.py
+++ project_remaining_time_on_task/models/project.py
@@ -5,26 +5,24 @@ from openerp import api, fields, models
 class Task(models.Model):
     _inherit = 'project.task'
 
-    remaining_contract_hours = fields.Float(string="Remaining contract hours",
-                                            compute='_compute_remaining_hours_contract',
-                                            digits=(16, 2))
+    has_contract_hours = fields.Boolean(string="Has contract hours",
+                                        compute="_compute_has_contract_hours")
+
+    remaining_contract_hours = fields.Float(
+        string="Remaining Contract Hours",
+        compute='_compute_remaining_hours_contract',
+        digits=(16, 2))
 
     def _get_qty_invoiced(self):
-        _qty_invoiced = 0
         sale_order_lines = self.env['sale.order.line'].search(
             [('project_id', '=', self.analytic_account_id.id)])
-        for line in sale_order_lines:
-            _qty_invoiced += line.qty_invoiced
 
-        return _qty_invoiced
+        return sum([line.qty_invoiced for line in sale_order_lines])
 
     def _get_qty_effective(self):
-        _qty_effective = 0
         tasks = self.project_id.task_ids
-        for task in tasks:
-            _qty_effective += task.effective_hours
 
-        return _qty_effective
+        return sum([task.effective_hours for task in tasks])
 
     @api.multi
     @api.depends('remaining_hours')
@@ -32,4 +30,14 @@ class Task(models.Model):
         _qty_invoiced = self._get_qty_invoiced()
         if _qty_invoiced > 0:
             for task in self:
-                task.remaining_contract_hours = _qty_invoiced - self._get_qty_effective()
+                task.remaining_contract_hours = _qty_invoiced \
+                                                - self._get_qty_effective()
+
+    @api.multi
+    @api.depends('has_contract_hours')
+    def _compute_has_contract_hours(self):
+        sale_order_lines = self.env['sale.order.line'].search(
+            [('project_id', '=', self.analytic_account_id.id)])
+
+        self.has_contract_hours = (sum(
+            [line.qty_invoiced for line in sale_order_lines])) > 0
