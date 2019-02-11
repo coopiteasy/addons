a6d35cf06c57129b0f0c6fe7e5b09257c2b3e834
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-02-11 16:29:53 +0100

    show provider reference

--- stock_provider_ref_on_receipt/models/stock.py
+++ stock_provider_ref_on_receipt/models/stock.py
@@ -2,7 +2,16 @@
 from openerp import api, fields, models
 
 
-class Stock(models.Model):
-    _inherit = 'stock.picking'
+class Product(models.Model):
+    _inherit = 'stock.pack.operation'
 
-    provider_ref_id = fields.Char(string="Provider Reference")
+    provider_ref = fields.Char(string="Provider Reference",
+                               compute="_compute_product_code")
+
+    @api.multi
+    @api.depends('provider_ref')
+    def _compute_product_code(self):
+        product_supplier = self.env['product.supplierinfo'].search(
+            [('product_tmpl_id', '=', self.product_id.product_tmpl_id.id)])
+        for line in self:
+            line.provider_ref = product_supplier[0].product_code
