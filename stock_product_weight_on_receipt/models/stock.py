fae7bd1e8a15c01490840a488e79d85f268b6751
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-02-18 11:55:11 +0100

    Add to test

--- stock_product_weight_on_receipt/models/stock.py
+++ stock_product_weight_on_receipt/models/stock.py
@@ -5,15 +5,4 @@ from openerp import api, fields, models
 class Product(models.Model):
     _inherit = 'stock.pack.operation'
 
-    # weight = fields.Float('Weight', related='product_id.weight')
-    display_weight = fields.Float(string="Display weight",
-                                  compute="_compute_weight")
-    # display_unit = fields.Float(string="Display unit")
-
-    @api.multi
-    # @api.depends('display_weight', 'display_unit')
-    @api.depends('display_weight')
-    def _compute_weight(self):
-        for line in self:
-            line.display_weight = line.product_id.display_weight
-            # line.display_unit = line.product_id.display_unit
+    display_weight = fields.Float('Weight', related='product_id.display_weight')
