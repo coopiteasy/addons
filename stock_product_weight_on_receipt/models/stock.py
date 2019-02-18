c4f703fa017234b92062ab0b8130b80115222f6b
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-02-18 16:37:51 +0100

    Add fields to PDF

--- stock_product_weight_on_receipt/models/stock.py
+++ stock_product_weight_on_receipt/models/stock.py
@@ -5,4 +5,11 @@ from openerp import api, fields, models
 class Product(models.Model):
     _inherit = 'stock.pack.operation'
 
-    display_weight = fields.Float('Weight', related='product_id.display_weight')
+    display_weight = fields.Float(string='Weight',
+                                  related='product_id.display_weight')
+
+    display_unit = fields.Many2one(
+        'product.uom',
+        'Weight Unit',
+        related='product_id.display_unit'
+    )
