95fba4b342b4ede9376b19926a8a093c511bb1d8
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-02-26 08:23:48 +0100

    Add files module

--- account_invoice_structured_communication/__openerp__.py
+++ account_invoice_structured_communication/__openerp__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Global discount on invoice',
+    'name': 'Partner Structured Communication',
     'version': '1.0',
     'author': 'Coop IT Easy SCRLfs',
     'category': 'Accounting',
