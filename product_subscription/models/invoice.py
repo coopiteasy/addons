0420a3812240d84d2c1b763fba964714b47cdcaf
houssine <houssine@DESKTOP-ICALFFT>
2019-02-26 12:25:45 +0100

    [REFACT] remove browse while the mail template is already on hand.

--- product_subscription/models/invoice.py
+++ product_subscription/models/invoice.py
@@ -2,16 +2,20 @@
 from datetime import datetime
 from openerp import models, fields, api
 
+
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
-    
-    subscription = fields.Boolean(string="Subscription")
-    product_subscription_request = fields.One2many('product.subscription.request','invoice', string="Product subscription")
 
-    def process_subscription(self,effective_date):
+    subscription = fields.Boolean(string="Subscription")
+    product_subscription_request = fields.One2many(
+                'product.subscription.request',
+                'invoice',
+                string="Product subscription")
+
+    def process_subscription(self, effective_date):
         # set the subscription request to paid
-        req_vals = {'state':'paid',
-                'payment_date':effective_date}
+        req_vals = {'state': 'paid',
+                    'payment_date': effective_date}
 
         sub_req = self.product_subscription_request
         sub_template = sub_req.subscription_template
@@ -22,8 +26,8 @@ class AccountInvoice(models.Model):
         if len(subscriber.subscriptions) > 0:
             # there is an existing subscription
             subscription = subscriber.subscriptions[0]
-            sub_vals = {'state':'ongoing',
-                        'counter':subscription.counter + sub_template.product_qty}
+            sub_vals = {'state': 'ongoing',
+                        'counter': subscription.counter + sub_template.product_qty}
             subscription.write(sub_vals)
             req_vals['subscription'] = subscription.id
         else:
@@ -31,14 +35,14 @@ class AccountInvoice(models.Model):
             prod_sub_seq = self.env.ref('product_subscription.sequence_product_subscription', False)
 
             prod_sub_num = prod_sub_seq.next_by_id()
-            sub_vals = {'name':prod_sub_num,
-                     'subscriber':subscriber.id,
-                     'subscribed_on':effective_date,
-                     'counter':sub_template.product_qty,
-                     'state':'ongoing'}
+            sub_vals = {'name': prod_sub_num,
+                        'subscriber': subscriber.id,
+                        'subscribed_on': effective_date,
+                        'counter': sub_template.product_qty,
+                        'state': 'ongoing'}
             subscription = self.env['product.subscription.object'].create(sub_vals)
             req_vals['subscription'] = subscription.id
-        subscriber.write({'subscriber':True,'old_subscriber':False})
+        subscriber.write({'subscriber': True, 'old_subscriber': False})
         sub_req.write(req_vals)
         # Send confirmation email
         self.send_confirm_paid_email()
@@ -48,14 +52,16 @@ class AccountInvoice(models.Model):
     def confirm_paid(self):
         for invoice in self:
             super(AccountInvoice, invoice).confirm_paid()
-            # we check if there is an open refund for this invoice. in this case we
-            # don't run the process_subscription function as the invoice has been 
-            # reconciled with a refund and not a payment.
-            refund = self.search([('type','=','out_refund'),('origin','=',invoice.move_name)])
-            
+            # we check if there is an open refund for this invoice. in this
+            # case we don't run the process_subscription function as the
+            # invoice has been reconciled with a refund and not a payment.
+            refund = self.search([('type', '=', 'out_refund'),
+                                  ('origin', '=', invoice.move_name)])
+
             if invoice.subscription and invoice.type == 'out_invoice' and not refund:
                 effective_date = datetime.now().strftime("%d/%m/%Y")
-                #take the effective date from the payment. by default the confirmation date is the payment date
+                # take the effective date from the payment.
+                # by default the confirmation date is the payment date
                 if invoice.payment_move_line_ids:
                     move_line = invoice.payment_move_line_ids[0]
                     effective_date = move_line.date
@@ -70,6 +76,5 @@ class AccountInvoice(models.Model):
             'product_subscription'
             '.subscription_payment_confirmation_email_template'
         )
-        (self.env['mail.template']
-         .browse(conf_email_template.id)
-         .send_mail(self.id))
+        for invoice in self:
+            conf_email_template.send_mail(invoice.id)
