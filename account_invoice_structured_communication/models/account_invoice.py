e17eec850a7d3f0d1ce06094b6446be58f6b16e4
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-02-28 09:42:37 +0100

    Ready for test

--- account_invoice_structured_communication/models/account_invoice.py
+++ account_invoice_structured_communication/models/account_invoice.py
@@ -5,12 +5,5 @@ from openerp import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    partner_structured_communication = fields.Char(string="Partner Structured "
-                                                          "Communication"
-                                                   )
-
-    @api.multi
-    def _compute_ref(self):
-        self.ensure_one()
-        if not self.check_bbacomm('partner_structured_communication'):
-            print("b is greater than a")
+    provider_reference = fields.Char(string="Provider reference invoice",
+                                     store=True)
