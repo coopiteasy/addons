79d22f8315be474beb4e2956ba2e27b267fa3111
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-03-03 13:09:24 +0100

    Add files and module

---
+++ account_archive_journals/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Coop IT Easy SCRLfs
+# Nicolas Jamoulle, <nicolas@coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Archive journals",
+    "version": "9.0.1.0",
+    "depends": [
+        'project',
+    ],
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "website": "www.coopiteasy.be",
+    "description": """
+        This module allow you to archive journals
+    """,
+    "data": [
+        'views/account_journal_view.xml',
+    ],
+    'installable': True,
+}
