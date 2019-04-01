07c9e19db981fadbcca84118466c45be18dd30f6
houssine <houssine@DESKTOP-ICALFFT>
2019-04-01 18:57:19 +0200

    [IMP] add code to correct ongoing subscriber with wrong status

--- product_subscription/models/product_release.py
+++ product_subscription/models/product_release.py
@@ -12,31 +12,63 @@ class ProductRelease(models.Model):
         for product_release in self:
             product_release.picking_ids = product_release.product_release_lines.mapped('picking')
             product_release.delivery_count = len(product_release.picking_ids)
-                
+
     name = fields.Char(string="Name", readonly=True, copy=False)
-    release_date = fields.Date(string='Product Release Date', readonly=True, required=True, index=True, states={'draft': [('readonly', False)]})
-    create_date = fields.Date(string='Creation Date', readonly=True, help="Date on which product release list is created.", copy=False, default=fields.Datetime.now)
-    
-    user_id = fields.Many2one('res.users', string='Release responsible', copy=False, default=lambda self: self.env.user)
-    product_id = fields.Many2one('product.product', string='Product', domain=[('sale_ok','=',True)], required=True, readonly=True, states={'draft': [('readonly', False)]})
-    product_release_lines = fields.One2many('product.release.line','product_release_list', string="Product release lines", copy=False)
-    release_qty = fields.Integer(string="Product release quantity", required=True, default=1)
-    
-    state = fields.Selection([
-        ('draft', 'Draft'),
-        ('validated', 'Validated'),
-        ('done', 'Done'),
-        ('cancelled', 'Cancelled'),
-        ], string='State', readonly=True, copy=False, default='draft')
-    
+    release_date = fields.Date(string='Product Release Date',
+                               readonly=True,
+                               required=True,
+                               index=True,
+                               states={'draft': [('readonly', False)]})
+    create_date = fields.Date(string='Creation Date',
+                              readonly=True,
+                              copy=False,
+                              default=fields.Datetime.now,
+                              help="Date on which product release list"
+                              " is created.")
+
+    user_id = fields.Many2one('res.users',
+                              string='Release responsible',
+                              copy=False,
+                              default=lambda self: self.env.user)
+    product_id = fields.Many2one('product.product',
+                                 string='Product',
+                                 domain=[('sale_ok', '=', True)],
+                                 required=True,
+                                 readonly=True,
+                                 states={'draft': [('readonly', False)]})
+    product_release_lines = fields.One2many('product.release.line',
+                                            'product_release_list',
+                                            string="Product release lines",
+                                            copy=False)
+    release_qty = fields.Integer(string="Product release quantity",
+                                 required=True,
+                                 default=1)
+
+    state = fields.Selection([('draft', 'Draft'),
+                              ('validated', 'Validated'),
+                              ('done', 'Done'),
+                              ('cancelled', 'Cancelled'),
+                              ],
+                             string='State',
+                             readonly=True,
+                             copy=False,
+                             default='draft')
+
     picking_policy = fields.Selection([
-        ('direct', 'Deliver each product when available'),
-        ('one', 'Deliver all products at once')],
-        string='Shipping Policy', required=True, readonly=True, default='direct',
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
-    picking_ids = fields.Many2many('stock.picking', compute='_compute_picking_ids', string='Picking associated to this release')
-    delivery_count = fields.Integer(string='Delivery Orders', compute='_compute_picking_ids')
-    
+                            ('direct', 'Deliver each product when available'),
+                            ('one', 'Deliver all products at once')],
+                            string='Shipping Policy',
+                            required=True,
+                            readonly=True,
+                            default='direct',
+                            states={'draft': [('readonly', False)],
+                                    'sent': [('readonly', False)]})
+    picking_ids = fields.Many2many('stock.picking',
+                                   compute='_compute_picking_ids',
+                                   string='Picking associated to this release')
+    delivery_count = fields.Integer(string='Delivery Orders',
+                                    compute='_compute_picking_ids')
+
     @api.multi
     def action_view_delivery(self):
         '''
@@ -67,106 +99,121 @@ class ProductRelease(models.Model):
             result['views'] = [(form_id, 'form')]
             result['res_id'] = pick_ids[0]
         return result
-    
+
     @api.one
     def action_draft(self):
         self.product_release_lines.unlink()
         self.state = 'draft'
-    
+
     @api.one
     def action_cancel(self):
         self.product_release_lines.unlink()
-        if self.state in ['draft','validated']:
+        if self.state in ['draft', 'validated']:
             self.state = 'cancelled'
-    
-    @api.one    
+
+    @api.one
     def action_validate(self):
         prod_sub_obj = self.env['product.subscription.object']
         release_line_obj = self.env['product.release.line']
-        
-        vals = {'state':'validated'}
+
+        vals = {'state': 'validated'}
         if self.name == '' or not self.name:
             prod_rel_seq = self.env.ref('product_subscription.sequence_product_release', False)
-            vals['name'] = prod_rel_seq.next_by_id()         
-        
-        subscriptions = prod_sub_obj.search([('counter','>',0)])
-        
-        line_vals = {'product_release_list':self.id,
-                'product_id':self.product_id.id}
-        
+            vals['name'] = prod_rel_seq.next_by_id()
+
+        subscriptions = prod_sub_obj.search([('counter', '>', 0)])
+
+        line_vals = {'product_release_list': self.id,
+                     'product_id': self.product_id.id}
+
         for subscription in subscriptions:
             line_vals['subscriber'] = subscription.subscriber.id
             line_vals['product_subscription'] = subscription.id
             release_line_obj.create(line_vals)
         self.write(vals)
-    
+
     def get_picking_vals(self, picking_type):
-        return {'picking_type_code':'outgoing',
-                'origin':self.name,
-                'move_type':self.picking_policy,
-                'picking_type_id':picking_type.id,
+        return {'picking_type_code': 'outgoing',
+                'origin': self.name,
+                'move_type': self.picking_policy,
+                'picking_type_id': picking_type.id,
                 'location_id': picking_type.default_location_src_id.id,
-                'location_dest_id':picking_type.default_location_dest_id.id}
-        
+                'location_dest_id': picking_type.default_location_dest_id.id}
+
     def get_stock_move_vals(self, picking_type):
-        return {'name':'/',
-            'product_id':self.product_id.id,
-            'product_uom':self.product_id.uom_id.id,
-            'product_uom_qty':self.release_qty,
-            'location_id': picking_type.default_location_src_id.id,
-            'location_dest_id':picking_type.default_location_dest_id.id}
-    
+        return {'name': '/',
+                'product_id': self.product_id.id,
+                'product_uom': self.product_id.uom_id.id,
+                'product_uom_qty': self.release_qty,
+                'location_id': picking_type.default_location_src_id.id,
+                'location_dest_id': picking_type.default_location_dest_id.id}
+
     @api.one
     def action_done(self):
-        picking_type = self.env['stock.picking.type'].search([('code','=','outgoing')])
+        picking_type = self.env['stock.picking.type'].search([('code', '=', 'outgoing')])
 
         for line in self.product_release_lines:
             if line.product_subscription.counter - self.release_qty >= 0:
                 picking_vals = self.get_picking_vals(picking_type)
                 stock_move_vals = self.get_stock_move_vals(picking_type)
-                line.create_picking(picking_vals,stock_move_vals)
+                line.create_picking(picking_vals, stock_move_vals)
                 line.product_subscription.counter = line.product_subscription.counter - self.release_qty
 
         subs_terminated = self.product_release_lines.filtered(lambda record: record.product_subscription.counter == 0)
         subs_renew = self.product_release_lines.filtered(lambda record: record.product_subscription.counter == 1)
-        
-        subs_terminated.write({'state':'terminated'})
+
+        subs_terminated.write({'state': 'terminated'})
         subscriber_terminated = subs_terminated.mapped('subscriber')
-        subscriber_terminated.write({'subscriber':False,'old_subscriber':True})
-        subs_renew.write({'state':'renew'})
-        
+        subscriber_terminated.write({'subscriber': False, 'old_subscriber': True})
+        subs_renew.write({'state': 'renew'})
+
         for picking in self.product_release_lines.mapped('picking'):
-            if picking.state not in ['cancel','done']:
+            if picking.state not in ['cancel', 'done']:
                 if picking.state != 'assigned':
                     picking.recheck_availability()
                     if picking.state != 'assigned':
-                        raise UserError(_('Not enough stock to deliver! Please check that there is sufficient product available'))
+                        raise UserError(_("Not enough stock to deliver! Please"
+                                          " check that there is sufficient"
+                                          " product available"))
                 for pack_operation in picking.pack_operation_ids:
                     if pack_operation.product_id.id == self.product_id.id:
                         pack_operation.qty_done = self.release_qty
                 picking.do_transfer()
         self.state = 'done'
-    
-        
+
+
 class ProductReleaseLine(models.Model):
     _name = "product.release.line"
-    
-    product_release_list = fields.Many2one('product.release.list', string="Product release list", required=True)
-    subscriber = fields.Many2one('res.partner', string="Subscriber", domain=[('subscriber','=',True)], required=True)
-    product_id = fields.Many2one('product.product', string='Product', required=True)
-    product_subscription = fields.Many2one('product.subscription.object', string='Subscription', required=True)
-    counter = fields.Float(related='product_subscription.counter', string="Counteur", readonly=True)
-    picking = fields.Many2one('stock.picking', string="Delivery order")
-    
-    @api.model    
+
+    product_release_list = fields.Many2one('product.release.list', 
+                                           string="Product release list",
+                                           required=True)
+    subscriber = fields.Many2one('res.partner',
+                                 string="Subscriber",
+                                 domain=[('subscriber', '=', True)],
+                                 required=True)
+    product_id = fields.Many2one('product.product',
+                                 string='Product',
+                                 required=True)
+    product_subscription = fields.Many2one('product.subscription.object',
+                                           string='Subscription',
+                                           required=True)
+    counter = fields.Float(related='product_subscription.counter',
+                           string="Counteur",
+                           readonly=True)
+    picking = fields.Many2one('stock.picking',
+                              string="Delivery order")
+
+    @api.model
     def create_picking(self, vals, stock_move_vals):
         picking_obj = self.env['stock.picking']
-         
+
         vals['partner_id'] = self.subscriber.id
-        
+
         picking = picking_obj.create(vals)
         stock_move_vals['picking_id'] = picking.id
         self.env['stock.move'].create(stock_move_vals)
-        
+
         self.picking = picking
+
         return picking
--- product_subscription/models/subscription.py
+++ product_subscription/models/subscription.py
@@ -6,56 +6,89 @@ from openerp.exceptions import UserError
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
-    
+
     firstname = fields.Char('First Name')
     lastname = fields.Char('Last Name')
     subscriber = fields.Boolean(string="Subscriber")
     old_subscriber = fields.Boolean(string="Old subscriber")
-    subscriptions = fields.One2many('product.subscription.object','subscriber', string="Subscription")
-    
+    subscriptions = fields.One2many('product.subscription.object',
+                                    'subscriber',
+                                    string="Subscription")
+
+
 class ProductTemplate(models.Model):
     _inherit = "product.template"
-    
+
     subscription = fields.Boolean(string="Subscription")
     product_qty = fields.Integer(string="Product quantity")
-    
+
+
 class SubscriptionTemplate(models.Model):
     _name = "product.subscription.template"
-    
-    name = fields.Char(string="Subscription name", copy=False, required=True)
+
+    name = fields.Char(string="Subscription name",
+                       copy=False,
+                       required=True)
     description = fields.Char(string="Description")
-    product_qty = fields.Integer(string="Subscription quantity", required=True, help="This is the quantity of product that will be allocated by this subscription")
-    price = fields.Float(related='product.lst_price', string="Subscription price", readonly=True)
+    product_qty = fields.Integer(string="Subscription quantity",
+                                 required=True,
+                                 help="This is the quantity of product that"
+                                 " will be allocated by this subscription")
+    price = fields.Float(related='product.lst_price',
+                         string="Subscription price",
+                         readonly=True)
     publish = fields.Boolean(string="Publish on website")
-    product = fields.Many2one('product.template', string='Product', 
-                              domain=[('subscription','=',True)],required=True)
-    analytic_distribution = fields.Many2one('account.analytic.distribution', string="Analytic distribution")
-    journal = fields.Many2one('account.journal',string='Journal',required=True,domain=[('type','=','sale')])
+    product = fields.Many2one('product.template', string='Product',
+                              domain=[('subscription', '=', True)],
+                              required=True)
+    analytic_distribution = fields.Many2one('account.analytic.distribution',
+                                            string="Analytic distribution")
+    journal = fields.Many2one('account.journal',
+                              string='Journal',
+                              required=True,
+                              domain=[('type', '=', 'sale')])
+
 
 class SubscriptionRequest(models.Model):
     _name = "product.subscription.request"
-    
+
     _order = "subscription_date desc, id desc"
-    
-    name = fields.Char(string="Name", copy=False)
+
+    name = fields.Char(string="Name",
+                       copy=False)
     gift = fields.Boolean(string="Gift?")
     is_company = fields.Boolean(string="Company?")
     sponsor = fields.Many2one('res.partner', string="Sponsor")
-    subscriber = fields.Many2one('res.partner', string="Subscriber", required=True)
-    subscription_date = fields.Date(string='Subscription request date', default=fields.Date.today())
+    subscriber = fields.Many2one('res.partner', string="Subscriber",
+                                 required=True)
+    subscription_date = fields.Date(string='Subscription request date',
+                                    default=fields.Date.today())
     payment_date = fields.Date(string="Payment date", readonly=True)
-    invoice = fields.Many2one('account.invoice', string="Invoice", readonly=True, copy=False)
-    state = fields.Selection([('draft','Draft'),
-                              ('sent','Sent'),
-                              ('paid','Paid'),
-                              ('cancel','Cancelled')], string="State", default="draft")
-    subscription = fields.Many2one('product.subscription.object', string="Subscription", readonly=True, copy=False)
-    subscription_template = fields.Many2one('product.subscription.template',string="Subscription template",required=True)
+    invoice = fields.Many2one('account.invoice',
+                              string="Invoice",
+                              readonly=True,
+                              copy=False)
+    state = fields.Selection([('draft', 'Draft'),
+                              ('sent', 'Sent'),
+                              ('paid', 'Paid'),
+                              ('cancel', 'Cancelled')],
+                             string="State",
+                             default="draft")
+    subscription = fields.Many2one('product.subscription.object',
+                                   string="Subscription",
+                                   readonly=True,
+                                   copy=False)
+    subscription_template = fields.Many2one('product.subscription.template',
+                                            string="Subscription template",
+                                            required=True)
 
     def _get_account(self, partner, product):
-        account = product.property_account_income_id or product.categ_id.property_account_income_categ_id
+        account = (product.property_account_income_id or
+                   product.categ_id.property_account_income_categ_id)
         if not account:
-            raise UserError(_('Please define income account for this product: "%s" (id:%d) - or for its category: "%s".') % \
+            raise UserError(_('Please define income account for this'
+                              ' product: "%s" (id:%d) - or for its'
+                              ' category: "%s".') %
                             (product.name, product.id, product.categ_id.name))
 
         fpos = partner.property_account_position_id
@@ -65,8 +98,7 @@ class SubscriptionRequest(models.Model):
 
     def _prepare_invoice_line(self, product, partner, qty):
         self.ensure_one()
-        res = {}
-        
+
         account = self._get_account(partner, product)
 
         res = {
@@ -79,70 +111,70 @@ class SubscriptionRequest(models.Model):
             'invoice_line_tax_ids': [(6, 0, product.taxes_id.ids)]
         }
         return res
-    
+
     def send_invoice(self, invoice):
         invoice_email_template = self.env.ref('account.email_template_edi_invoice', False)
-        
-        # we send the email with invoice in attachment 
+
+        # we send the email with invoice in attachment
         invoice_email_template.send_mail(invoice.id)
         invoice.sent = True
-        
+
     def create_invoice(self, partner, vals={}):
         # creating invoice and invoice lines
-        vals.update({'partner_id':partner.id, 
-                'subscription':True,
-                'journal_id':self.subscription_template.journal.id,
-                'type': 'out_invoice'})
-        
+        vals.update({'partner_id': partner.id,
+                     'subscription': True,
+                     'journal_id': self.subscription_template.journal.id,
+                     'type': 'out_invoice'})
+
         invoice = self.env['account.invoice'].create(vals)
-        
+
         vals = self._prepare_invoice_line(self.subscription_template.product, partner, 1)
         vals['invoice_id'] = invoice.id
-        
+
         if self.subscription_template.analytic_distribution:
             vals['analytic_distribution_id'] = self.subscription_template.analytic_distribution.id
-        
-        line = self.env['account.invoice.line'].create(vals)
-        
+
+        self.env['account.invoice.line'].create(vals)
+
         return invoice
-    
+
     @api.model
-    def create(self,vals):
+    def create(self, vals):
         prod_sub_req_seq = self.env.ref('product_subscription.sequence_product_subscription_request', False)
-        
+
         prod_sub_num = prod_sub_req_seq.next_by_id()
         vals['name'] = prod_sub_num
-        
-        return super(SubscriptionRequest,self).create(vals)
-    
+
+        return super(SubscriptionRequest, self).create(vals)
+
     @api.one
     def validate_request(self):
         partner = self.subscriber
         # if it's a gift then the sponsor is set on the invoice
         if self.gift:
             partner = self.sponsor
-        
-        invoice = self.create_invoice(partner,{})
+
+        invoice = self.create_invoice(partner, {})
         invoice.compute_taxes()
-        
+
         invoice.signal_workflow('invoice_open')
-        
+
         self.send_invoice(invoice)
-        
-        self.write({'state':'sent','invoice':invoice.id})
+
+        self.write({'state': 'sent', 'invoice': invoice.id})
 
     @api.one
     def cancel_request(self):
         self.state = 'cancel'
-        
+
     @api.one
     def action_draft(self):
         self.state = 'draft'
-    
+
     @api.model
     def _validate_pending_request(self):
-        pending_request_list = self.search([('state','=','draft')])
-        
+        pending_request_list = self.search([('state', '=', 'draft')])
+
         for pending_request in pending_request_list:
             try:
                 pending_request.validate_request()
@@ -152,26 +184,46 @@ class SubscriptionRequest(models.Model):
 
 class SubscriptionObject(models.Model):
     _name = "product.subscription.object"
-    
+
     @api.model
     def _compute_subscriber(self):
-        sub_to_renew = self.search([('counter','=',1),('state','!=','renew')])
-        sub_to_renew.write({'state':'renew'})
-        sub_to_terminate = self.search([('counter','=',0),('state','!=','terminated')])
-        sub_to_terminate.write({'state':'terminated'})
-        
-        subscribers = self.search([('state','=','terminated')]).mapped('subscriber')
+        sub_to_renew = self.search([('counter', '=', 1),
+                                    ('state', '!=', 'renew')])
+        sub_to_renew.write({'state': 'renew'})
+        sub_to_terminate = self.search([('counter', '=', 0),
+                                        ('state', '!=', 'terminated')])
+        sub_to_terminate.write({'state': 'terminated'})
+
+        subscribers = self.search([('state', '=', 'terminated')]).mapped('subscriber')
         to_deactivate = subscribers.filtered('subscriber')
+
         if len(to_deactivate) > 0:
-            to_deactivate.write({'subscriber':False, 'old_subscriber':True})
-        
-    name = fields.Char(string="Name", copy=False, required=True)
-    subscriber = fields.Many2one('res.partner', string="Subscriber", required=True)
+            to_deactivate.write({'subscriber': False,
+                                 'old_subscriber': True})
+        # this part is to eventual delta between the subscriber status
+        # and the corresponding subscription status
+        ongoing_subscription = self.search(['counter', '>', 0])
+        ongoing_subscriber = ongoing_subscription.mapped('subscriber')
+        subscriber_wrong_status = ongoing_subscriber.filtered('old_subscriber')
+        if subscriber_wrong_status:
+            subscriber_wrong_status.write({'subscriber': True,
+                                           'old_subscriber': False})
+
+    name = fields.Char(string="Name",
+                       copy=False,
+                       required=True)
+    subscriber = fields.Many2one('res.partner',
+                                 string="Subscriber",
+                                 required=True)
     counter = fields.Float(string="Counter")
     subscribed_on = fields.Date(string="First subscription date")
-    state = fields.Selection([('draft','Draft'),
-                              ('waiting','Waiting'),
-                              ('ongoing','Ongoing'),
-                              ('renew','Need to Renew'),
-                              ('terminated','Terminated')],string="State", default="draft")
-    subscription_requests = fields.One2many('product.subscription.request','subscription', string="Subscription request")
+    state = fields.Selection([('draft', 'Draft'),
+                              ('waiting', 'Waiting'),
+                              ('ongoing', 'Ongoing'),
+                              ('renew', 'Need to Renew'),
+                              ('terminated', 'Terminated')],
+                             string="State",
+                             default="draft")
+    subscription_requests = fields.One2many('product.subscription.request',
+                                            'subscription',
+                                            string="Subscription request")
