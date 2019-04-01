9c1b04db67037ced9fa01672208d5e4dabdf41c3
houssine <houssine@DESKTOP-ICALFFT>
2019-04-01 16:04:08 +0200

    [ADD] add module res_users_case_insensitive

---
+++ res_users_case_insensitive/models/res_users.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# © 2015-TODAY LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, api, fields
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+    login = fields.Char(
+        'Login',
+        size=64,
+        required=True,
+        help='Used to log into the system. Case insensitive.',
+    )
+
+    @api.model
+    def search(self, domain, *args, **kwargs):
+        """
+        Overload search to lowercase name domains. Can't do in a typical
+        search method due to the field not being computed
+        """
+        for idx, _domain in enumerate(domain):
+            if _domain[0] == 'login':
+                lower = _domain[2].lower() if _domain[2] else False
+                domain[idx] = (_domain[0], _domain[1], lower)
+        return super(ResUsers, self).search(domain, *args, **kwargs)
+
+    @api.model
+    def create(self, vals, ):
+        """ Overload create to lowercase login """
+        vals['login'] = vals.get('login', '').lower()
+        return super(ResUsers, self).create(vals)
+
+    @api.multi
+    def write(self, vals, ):
+        """ Overload write to lowercase login """
+        if vals.get('login'):
+            vals['login'] = vals['login'].lower()
+        return super(ResUsers, self).write(vals)
---
+++ res_users_case_insensitive/tests/test_res_users.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# © 2015-TODAY LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestResUsers(TransactionCase):
+
+    def setUp(self):
+        super(TestResUsers, self).setUp()
+        self.login = 'LasLabs@ExAmPlE.CoM'
+        self.partner_vals = {
+            'name': 'Partner',
+            'is_company': False,
+            'email': self.login,
+        }
+        self.vals = {
+            'name': 'User',
+            'login': self.login,
+            'password': 'password',
+        }
+        self.model_obj = self.env['res.users']
+
+    def _new_record(self, ):
+        partner_id = self.env['res.partner'].create(self.partner_vals)
+        self.vals['partner_id'] = partner_id.id
+        return self.model_obj.create(self.vals)
+
+    def test_login_is_lowercased_on_create(self, ):
+        rec_id = self._new_record()
+        self.assertEqual(
+            self.login.lower(), rec_id.login,
+            'Login was not lowercased when saved to db.',
+        )
+
+    def test_login_is_lowercased_on_write(self, ):
+        rec_id = self._new_record()
+        rec_id.write({'login': self.login})
+        self.assertEqual(
+            self.login.lower(), rec_id.login,
+            'Login was not lowercased when saved to db.',
+        )
+
+    def test_login_search_is_lowercased(self, ):
+        rec_id = self._new_record()
+        res_id = self.model_obj.search([('login', '=', self.login.upper())])
+        res = res_id.id if res_id else False
+        self.assertEqual(
+            rec_id.id, res,
+            'Search for login with uppercase chars did not yield results.',
+        )
