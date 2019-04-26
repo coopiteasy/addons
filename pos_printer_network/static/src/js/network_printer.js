df9b24f243ae8e31462b122bd44a5cd09ec78542
houssine <houssine@DESKTOP-ICALFFT>
2019-04-26 17:02:35 +0200

    [CLEAN] remove cause it's already set in an existing extension

--- pos_printer_network/static/src/js/network_printer.js
+++ pos_printer_network/static/src/js/network_printer.js
@@ -352,34 +352,4 @@ odoo.define('pos_printer_network.network_printer', function (require) {
         },
     });
     gui.define_popup({name:'proxy_printers', widget: ProxyPrintersPopupWidget});
-    
-    module.ReceiptScreenWidget = module.ReceiptScreenWidget.include({
-        send : function() {
-            var self = this;
-            var loaded = new $.Deferred();
-            var order = this.pos.get_order().name;
-            var records = new Model('pos.order').call('send_order', [order], {}, { shadow: false, timeout: 10000});
-            records.then(function(result){
-                var el = self.$('.message-send')
-                el.empty();
-                el.append('<h2>' + result + '</h2>');
-            },function(err){
-                loaded.reject(err);
-            });
-        },
-        renderElement: function() {
-            var self = this;
-            this._super();
-            this.$('.button.send').click(function(){
-                if (!self._locked) {
-                    self.send();
-                }
-            });
-        },
-        show: function(){
-            this._super();
-            var self = this;
-            this.$('.message-send').empty();
-        },
-    })
 });
