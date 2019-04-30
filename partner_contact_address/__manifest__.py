8e73259e4ffa094d8fbabd79d7a6d762e0236a51
houssine <houssine.bakkali@gmail.com>
2019-04-30 15:03:00 +0200

    [PEP8] fix pep8
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- partner_contact_address/__manifest__.py
+++ partner_contact_address/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2018-Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
-# - Houssine BAKKALI - <houssine@coopiteasy.be> 
+# - Houssine BAKKALI - <houssine@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Partner contact address",
