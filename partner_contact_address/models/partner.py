b097fcb745c7e75faeff845d2320a3e775db22e1
houssine <houssine.bakkali@gmail.com>
2019-04-30 12:41:58 +0200

    [MIG] migration to v12

--- partner_contact_address/models/partner.py
+++ partner_contact_address/models/partner.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Coop IT Easy SCRLfs.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from openerp import fields, models
 
@@ -9,6 +8,3 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     type = fields.Selection(selection_add=[('representative', 'Representative')], default="representative")
-
-
-
