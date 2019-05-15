203f8488704d806000d2436106ce055821efc257
robin.keunen <robin@keunen.net>
2019-05-15 15:50:02 +0200

    [ADD] provelo_c: provelo custom res_groups

--- provelo_customization/__openerp__.py
+++ provelo_customization/__openerp__.py
@@ -21,6 +21,7 @@
     "name": "Provelo Customization",
     "version": "9.0.1.0",
     "depends": [
+        'resource_planning',
         'resource_activity',
     ],
     "author": "Coop IT Easy - Robin Keunen <robin@coopiteasy.be>",
@@ -33,6 +34,8 @@
     'data': [
         'views/location_filters.xml',
         'views/res_partner_views.xml',
+        'security/custom_security.xml',
+        'security/ir.model.access.csv',
     ],
     'installable': True,
 }
