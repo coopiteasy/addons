6ab3fe560617efe26f8b86bd65798531d8504c86
houssine <houssine.bakkali@gmail.com>
2019-08-02 11:32:58 +0200

    [IMP] add options update and print into log

--- mrp_brewing/wizard/recompute_qty_after_move.py
+++ mrp_brewing/wizard/recompute_qty_after_move.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from openerp import api, models
+from openerp import api, fields, models
 
 
 class StockRecomputeAfterMove(models.TransientModel):
     _name = "stock.recompute.after.move"
+
+    update = fields.Boolean(string="Update")
+    print_log = fields.Boolean(string="Print computing in the log")
 
     @api.multi
     def recompute(self):
@@ -21,7 +24,8 @@ class StockRecomputeAfterMove(models.TransientModel):
                                 ('product_id', '=', product.id)
                                ], order="date asc")
             qty_after_move = 0
-            print ("=========== product = " + product.name + " ============")
+            if self.print_log:
+                print ("========= product = " + product.name + " ==========")
             for move in moves:
                 qty = 0
                 if move.location_id.usage == 'customer' \
@@ -32,18 +36,26 @@ class StockRecomputeAfterMove(models.TransientModel):
                     qty = move.product_qty
                 elif move.location_dest_id.usage == 'transit':
                     qty = -move.product_qty
-                elif move.location_dest_id.usage in ['inventory', 'production', 'customer']:
+                elif move.location_dest_id.usage in ['inventory',
+                                                     'production',
+                                                     'customer'
+                                                     ]:
                     qty = -move.product_qty
-                elif move.location_id.usage in ['inventory', 'production', 'internal']:
+                elif move.location_id.usage in ['inventory',
+                                                'production',
+                                                'internal'
+                                                ]:
                     qty = move.product_qty
 
                 qty_after_move += qty
-                print ("move " + str(move.origin) +
-                       " from " + move.location_id.name +
-                       " to " + move.location_dest_id.name +
-                       # " for " + move.partner_id.name +
-                       " on " + str(move.date) +
-                       " moved qty is " + str(qty) + " qty after move "
-                       "is " + str(move.quantity_after_move) +
-                       " instead of " + str(qty_after_move))
+                if self.print_log:
+                    print ("move " + str(move.origin) +
+                           " from " + move.location_id.name +
+                           " to " + move.location_dest_id.name +
+                           " on " + str(move.date) +
+                           " moved qty is " + str(qty) + " qty after move "
+                           "is " + str(move.quantity_after_move) +
+                           " instead of " + str(qty_after_move))
+                if self.update:
+                    move.quantity_after_move = qty_after_move
         return True
