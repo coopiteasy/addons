849320f13ad20b3f8b2a3cd98ebdcda110b24d15
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2019-10-08 16:53:17 +0200

    [FIX] linting, attribution and cleanup

--- res_partner_bank_remove_uniqueness_constraint/__manifest__.py
+++ res_partner_bank_remove_uniqueness_constraint/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2013-2018 Open Architects Consulting SPRL.
 # Copyright 2018-Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
-# - Houssine BAKKALI - <houssine@coopiteasy.be>
+# - Manuel Claeys Bouuaert - <manuel@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).#
 
 {
@@ -14,7 +14,5 @@
     This module removes the SQL uniqueness constraint in the Bank Account model
     res.partner.bank that requires every bank account to have a unique
     (sanitized) account number.
-    """,
-    'data': [
-    ]
+    """
 }
