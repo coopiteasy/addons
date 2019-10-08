2ba7fc0642a9902d88a8c4a681cf29799730c8cf
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2019-10-08 11:44:28 +0200

    [ADD] initial commit

---
+++ res_partner_bank_remove_uniqueness_constraint/models/res_partner_bank_remove_uniqueness_constraint.py
@@ -0,0 +1,10 @@
+from odoo import models
+
+
+class ResPartnerBank(models.Model):
+    _inherit = 'res.partner.bank'
+
+    _sql_constraints = [
+        ('unique_number', 'Check(1=1)', 'Account Number must be unique!'),
+        # Always returns true, effectively removing the constraint.
+    ]
