cb72e7b37fe87e303491f9b7821e964cc42b584d
robin.keunen <robin@keunen.net>
2019-10-29 11:58:33 +0100

    [REF] blacken logeco custom

--- logeco_custom/models/housing.py
+++ logeco_custom/models/housing.py
@@ -2,8 +2,6 @@ from odoo import fields, models
 
 
 class Housing(models.Model):
-    _inherit = 'hc.housing'
+    _inherit = "hc.housing"
 
-    floor_oclp = fields.Integer(
-        string='OCLP Floor number',
-        required=False)
+    floor_oclp = fields.Integer(string="OCLP Floor number", required=False)
