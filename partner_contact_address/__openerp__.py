db7006f05d69dc6da0b93e9104b70639a87ba588
houssine <houssine.bakkali@gmail.com>
2019-11-14 16:12:04 +0100

    [IMP] fix pep8, version and copyright

--- partner_contact_address/__openerp__.py
+++ partner_contact_address/__openerp__.py
@@ -1,28 +1,12 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Business Open Source Solution
-#    Copyright (C) 2018- Coop IT Easy SCRL.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2019 Houssine BAKKALI, Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     "name": "Partner contact address",
-    "version": "1.0",
+    "version": "9.0.1.0.0",
     "depends": ["base"],
-    "author": "Houssine BAKKALI <houssine@coopiteasy.be>",
+    "author": "Coop IT Easy SCRLfs>",
     "category": "CRM",
     "website": "www.coopiteasy.be",
     "license": "AGPL-3",
@@ -30,7 +14,6 @@
     This module allows to have company contacts with their own address.
     """,
     'data': [
-      #  'views/partner_views.xml',
     ],
     'installable': True,
     'application': False,
--- partner_contact_address/models/partner.py
+++ partner_contact_address/models/partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 Coop IT Easy SCRLfs.
+# © 2019 Houssine BAKKALI, Coop IT Easy SCRLfs
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import fields, models
@@ -8,7 +8,7 @@ from openerp import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    type = fields.Selection(selection_add=[('representative', 'Representative')], default="representative")
-
-
-
+    type = fields.Selection(selection_add=[
+                        ('representative', 'Representative')
+                        ],
+                        default="representative")
