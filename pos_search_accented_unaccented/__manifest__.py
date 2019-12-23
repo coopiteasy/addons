f49a7ed33e8371b02270bef919e6b915817e551a
Vincent Van Rossem <vvrossem@gmail.com>
2019-12-23 11:07:18 +0100

    [MIG] pos_search_accented_unaccented: 10.0 > 11.0 followed OCA guidelines, not tested on 11.0

--- pos_search_accented_unaccented/__manifest__.py
+++ pos_search_accented_unaccented/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Coop IT Easy SCRLfs
 #   - Vincent Van Rossem <vincent@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "POS Search Accented/Unaccented Characters",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "depends": [
         "point_of_sale",
     ],
