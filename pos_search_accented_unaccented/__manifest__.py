b25678e3fe1623cc2d537e4ff1d1c015c79bc547
Vincent Van Rossem <vvrossem@gmail.com>
2019-12-23 11:03:40 +0100

    [MIG] pos_search_accented_unaccented: 9.0 > 10.0 followed OCA guidelines, not tested on 10.0

--- pos_search_accented_unaccented/__manifest__.py
+++ pos_search_accented_unaccented/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "POS Search Accented/Unaccented Characters",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "depends": [
         "point_of_sale",
     ],
