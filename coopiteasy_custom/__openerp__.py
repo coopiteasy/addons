f40eca267dda235618c8209608bbbee0b1137732
robin.keunen <robin@keunen.net>
2019-12-24 13:32:50 +0100

    [ADD] cie_custom: fields on tasks
    blacken coopiteasy custom
    
    wip

--- coopiteasy_custom/__openerp__.py
+++ coopiteasy_custom/__openerp__.py
@@ -1,38 +1,19 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2017- Coop IT Easy.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
 {
     "name": "Coop IT Easy Customization",
     "version": "9.0.1.0",
-    "depends": [
-        'project',
-    ],
-    "author": "Coop IT Easy - Robin Keunen <robin@coopiteasy.be>",
+    "depends": ["analytic", "project"],
+    "author": "Coop IT Easy SCRLfs",
     "license": "AGPL-3",
     "category": "",
     "website": "www.coopiteasy.be",
-    "description": """
+    "summary": """
         Specifics customizations for Coop IT Easy
     """,
-    'data': [
-        'data/cron.xml',
-        'views/project_view.xml',
-    ],
-    'installable': True,
+    "data": ["data/cron.xml", "views/project_view.xml"],
+    "installable": True,
 }
--- coopiteasy_custom/models/__init__.py
+++ coopiteasy_custom/models/__init__.py
@@ -1,2 +1,2 @@
-from . import project
 from . import sale_timesheet
+from . import task
