de57bf7347a421a1548bba59c46fba8139f4c6d1
robin.keunen <robin@keunen.net>
2019-12-24 13:32:50 +0100

    [ADD] cie_custom: fields on tasks
    blacken coopiteasy custom
    
    wip
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- coopiteasy_custom/models/sale_timesheet.py
+++ coopiteasy_custom/models/sale_timesheet.py
@@ -7,20 +7,20 @@ from openerp import api, fields, models, _
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     @api.model
     def reset_so_line(self):
-        lines = self.search([('so_line.order_id.state', '=', 'sale')])
+        lines = self.search([("so_line.order_id.state", "=", "sale")])
         for line in lines:
             vals = {
-                'product_uom_id': line.product_uom_id.id,
-                'amount': -0.0,
-                'account_id': line.account_id.id,
+                "product_uom_id": line.product_uom_id.id,
+                "amount": -0.0,
+                "account_id": line.account_id.id,
             }
             res = line.sudo()._get_sale_order_line(vals=vals)
             line.write(vals)
             print(res)
 
-        lines.mapped('so_line').sudo()._compute_analytic()
+        lines.mapped("so_line").sudo()._compute_analytic()
         return lines
