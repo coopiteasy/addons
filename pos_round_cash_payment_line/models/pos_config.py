4e47e60ecdc178aafe0b9f7e23cc4dc09eaee6fa
houssine <houssine.bakkali@gmail.com>
2020-01-14 20:04:38 +0100

    [IMP] add contributo

--- pos_round_cash_payment_line/models/pos_config.py
+++ pos_round_cash_payment_line/models/pos_config.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # ?? 2016 Robin Keunen, Coop IT Easy SCRL fs
+# ?? 2020 Houssine Bakkali, Coop IT Easy SCRL fs
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields
