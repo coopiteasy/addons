4156e2008a0447e89526920eea8d18d75f543c4d
houssine <houssine.bakkali@gmail.com>
2020-01-15 16:09:15 +0100

    [DESC] add description

--- pos_round_cash_payment_line/__openerp__.py
+++ pos_round_cash_payment_line/__openerp__.py
@@ -14,7 +14,7 @@
         Rounds due amount to nearest 5 cents when adding a cash Payment line.
         An line is added on the invoice to record the rounding remainder.
 
-        A Round Remainder journal must be defined.
+        A Round Remainder journal must be created and assigned to the POS.
 
     """,
     "depends": [
