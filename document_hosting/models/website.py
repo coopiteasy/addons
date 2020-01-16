21afdb66a97241ffc8735e65796dd26a5776235a
Elouan Le Bars <elouan@coopiteasy.be>
2020-01-16 14:10:53 +0100

    [ADD] document_hosting : display on website configurable
    Menu is still displayed, regardless of the setting.

---
+++ document_hosting/models/website.py
@@ -0,0 +1,13 @@
+# Copyright 2018 - Today Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import api, fields, models, tools
+
+
+class Website(models.Model):
+    _inherit = "website"
+
+    display_document_page = fields.Boolean(
+        "Display documents on website", default=True
+    )
