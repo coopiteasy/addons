d6f78b869d1bc95e9cd794c6efe4d3077efb128a
Elouan Le Bars <elouan@coopiteasy.be>
2020-01-16 14:10:53 +0100

    [ADD] document_hosting : display on website configurable
    Menu is still displayed, regardless of the setting.

---
+++ document_hosting/models/res_config_settings.py
@@ -0,0 +1,13 @@
+# Copyright 2018 - Today Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = "res.config.settings"
+
+    display_document_page = fields.Boolean(
+        related="website_id.display_document_page", readonly=False
+    )
