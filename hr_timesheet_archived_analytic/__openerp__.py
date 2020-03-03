75aa336a21bdad587248b425abb7c3c5918331e9
Vincent Van Rossem <vvrossem@gmail.com>
2020-03-03 12:56:20 +0100

    [IMP] hr_timesheet_archived_analytic: domain for aal

--- hr_timesheet_archived_analytic/__openerp__.py
+++ hr_timesheet_archived_analytic/__openerp__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 Coop IT Easy SCRLfs
 #     - Rémy Taymans <remy@coopiteasy.be>
+#     - Vincent Van Rossem <vincent@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Hr Timesheet Archived Analytic',
     'description': """
         Hide archived analytic account in the timesheet form.""",
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'license': 'AGPL-3',
     'author': 'Coop IT Easy SCRLfs',
     'website': 'https://coopiteasy.be',
@@ -15,6 +16,7 @@
         'hr_timesheet_sheet',
     ],
     'data': [
-        'views/hr_timesheet_sheet.xml',
+        'views/hr_timesheet_sheet_view.xml',
+        'views/hr_timesheet_view.xml',
     ],
 }
