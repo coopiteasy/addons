ae58ddaf77dd9b20d1f65a37e742ae696a213462
Vincent Van Rossem <vvrossem@gmail.com>
2020-03-05 11:32:44 +0100

    [REF] hr_timesheet_archived_analytic: correct year + black format

--- hr_timesheet_archived_analytic/__openerp__.py
+++ hr_timesheet_archived_analytic/__openerp__.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 Coop IT Easy SCRLfs
+# Copyright 2020 Coop IT Easy SCRLfs
 #     - Rémy Taymans <remy@coopiteasy.be>
 #     - Vincent Van Rossem <vincent@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Hr Timesheet Archived Analytic',
-    'description': """
+    "name": "Hr Timesheet Archived Analytic",
+    "description": """
         Hide archived analytic account in the timesheet form.""",
-    'version': '9.0.1.1.0',
-    'license': 'AGPL-3',
-    'author': 'Coop IT Easy SCRLfs',
-    'website': 'https://coopiteasy.be',
-    'depends': [
-        'hr_timesheet_sheet',
-    ],
-    'data': [
-        'views/hr_timesheet_sheet_view.xml',
-        'views/hr_timesheet_view.xml',
+    "version": "9.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "Coop IT Easy SCRLfs",
+    "website": "https://coopiteasy.be",
+    "depends": ["hr_timesheet_sheet",],
+    "data": [
+        "views/hr_timesheet_sheet_view.xml",
+        "views/hr_timesheet_view.xml",
     ],
 }
