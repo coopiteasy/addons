a6369ed06ade641da400979ca7768a5f1bb35303
Vincent Van Rossem <vvrossem@gmail.com>
2020-04-09 10:59:23 +0200

    [MIG] purchase_order_line_auto_import: migration to 11.0 - followed oca guidelines - not tested on odoo 11.0

--- purchase_order_line_auto_import/__manifest__.py
+++ purchase_order_line_auto_import/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2016 Houssine BAKKALI, Coop IT Easy SCRL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
@@ -10,7 +9,7 @@
     """,
     'author': 'Houssine BAKKALI, Coop IT Easy SCRL',
     'category': 'Purchase Management',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'depends': ['purchase',],
     'data': [
