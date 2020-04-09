dd3dbe42688b63ad5633ef57ff2054c7d2746652
Vincent Van Rossem <vvrossem@gmail.com>
2020-04-09 11:27:02 +0200

    [MIG] purchase_order_line_auto_import: remove unused import

--- purchase_order_line_auto_import/models/purchase.py
+++ purchase_order_line_auto_import/models/purchase.py
@@ -2,11 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 from odoo import api, fields, models, SUPERUSER_ID
-from odoo.exceptions import Warning as UserError
-from odoo.tools.translate import _
 import openerp.addons.decimal_precision as dp
 
 UNIT = dp.get_precision('Product Unit of Measure')
