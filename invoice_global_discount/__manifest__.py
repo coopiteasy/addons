2f3baddcbce5a1ea06d01150a19c5304311dca49
Vincent Van Rossem <vvrossem@gmail.com>
2020-04-15 14:47:02 +0200

    [MIG] invoice_global_discount: migration to 11.0 - followed oca guidelines - not tested on odoo 11.0

--- invoice_global_discount/__manifest__.py
+++ invoice_global_discount/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Houssine BAKKALI <houssine@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Global discount on invoice',
-    'version': '10.0.1.1.0',
+    'version': '11.0.1.1.0',
     'author': 'Houssine BAKKALI - Coop IT Easy',
     'category': 'Accounting',
     'license': 'AGPL-3',
