2796beac97dfddc21240661c1784649c156ad0ad
Vincent Van Rossem <vvrossem@gmail.com>
2020-04-15 15:21:09 +0200

    [IMP] invoice_global_discount: black formatter

--- invoice_global_discount/__manifest__.py
+++ invoice_global_discount/__manifest__.py
@@ -3,20 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Global discount on invoice',
-    'version': '12.0.1.1.0',
-    'author': 'Coop IT Easy SCRLfs',
-    'category': 'Accounting',
-    'license': 'AGPL-3',
-    'website': 'https://coopiteasy.be',
-    'depends': [
-        'account', 'sale'
-    ],
+    "name": "Global discount on invoice",
+    "version": "12.0.1.1.0",
+    "author": "Coop IT Easy SCRLfs",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "website": "https://coopiteasy.be",
+    "depends": ["account", "sale"],
     "description": """
-    this module give global discount on invoice. It allows to set a the same discount on all the invoice line without been forced to go manually through all the lines.    
+    This module give global discount on invoice.
+    It allows to set the same discount on all the invoice lines without been forced to go manually through them.    
     """,
-    'data': [
-        'views/account_invoice_view.xml'
-    ],
-    'installable': True,
+    "data": ["views/account_invoice_view.xml"],
+    "installable": True,
 }
--- invoice_global_discount/models/account_invoice.py
+++ invoice_global_discount/models/account_invoice.py
@@ -4,14 +4,18 @@ from odoo.addons import decimal_precision as dp
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
-    
-    global_discount = fields.Float(string='Global discount(%)', digits=dp.get_precision('Discount'), default=0.0)
-    
+    _inherit = "account.invoice"
+
+    global_discount = fields.Float(
+        string="Global discount(%)",
+        digits=dp.get_precision("Discount"),
+        default=0.0,
+    )
+
     @api.multi
     def propagate_discount_on_lines(self):
         self.ensure_one()
         for line in self.invoice_line_ids:
             line.discount = self.global_discount
-        
+
         self.compute_taxes()
