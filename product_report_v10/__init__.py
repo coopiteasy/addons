f53f422b7b6658471664b80c94a7c68fd6932854
Vincent Van Rossem <vvrossem@gmail.com>
2020-04-16 11:23:04 +0200

    [MIG] product_report_v10: migration to 11.0 - followed oca guidelines - not tested on odoo 11.0

--- product_report_v10/__init__.py
+++ product_report_v10/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
 # © 2017 Houssine BAKKALI, Coop IT Easy
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
--- product_report_v10/__manifest__.py
+++ product_report_v10/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2016 Houssine BAKKALI, Open Architects Consulting SPRL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
@@ -8,10 +7,10 @@
     """,
     'author': 'Houssine BAKKALI, Coop IT Easy',
     'category': 'Product',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'website': 'www.coopiteasy.be',
     'license': 'AGPL-3',
-    'depends': ['base', 'product', 'report'],
+    'depends': ['base', 'product', 'web'],
     'data': [
         'report/layout.xml',
         'report/product_template_label_repeat.xml',
