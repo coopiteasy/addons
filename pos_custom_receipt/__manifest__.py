793d1f5afddbb049920fdb81c4593ed2d1b1bc5d
Vincent Van Rossem <vvrossem@gmail.com>
2020-04-28 16:40:20 +0200

    [MIG] pos_custom_receipt: migration to 12.0 - followed oca guidelines

--- pos_custom_receipt/__manifest__.py
+++ pos_custom_receipt/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "POS Custom Receipt",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "depends": [
         "point_of_sale",
     ],
