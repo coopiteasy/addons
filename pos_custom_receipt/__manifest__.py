c5c79831674d6b831b14e55d41c35eb8f53c98d0
Vincent Van Rossem <vvrossem@gmail.com>
2020-04-28 16:38:22 +0200

    [MIG] pos_custom_receipt: migration to 11.0 - followed oca guidelines - not tested on odoo 11.0

--- pos_custom_receipt/__manifest__.py
+++ pos_custom_receipt/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019-Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
 # - Houssine BAKKALI - <houssine@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "POS Custom Receipt",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "depends": [
         "point_of_sale",
     ],
