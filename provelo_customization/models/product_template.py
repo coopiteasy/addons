b5d712f6907f284d4b32770ebb4d0c876f3bf6ae
robin.keunen <robin@keunen.net>
2020-06-03 17:03:36 +0200

    [ADD] pv_custom: default invoice_policy to order

---
+++ provelo_customization/models/product_template.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    invoice_policy = fields.Selection(default="order")
