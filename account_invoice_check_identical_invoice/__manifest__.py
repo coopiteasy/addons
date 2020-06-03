1a824acc0d1d61e3b26cd894942567bab88b7538
Vincent Van Rossem <vvrossem@gmail.com>
2020-06-03 17:22:40 +0200

    [MIG] account_invoice_check_identical_invoice: 9.0 -> 10.0 - followed oca guidelines - not tested on odoo 10.0

---
+++ account_invoice_check_identical_invoice/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 - Today Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Check Identical Invoice",
+    "version": "10.0.1.0.0",
+    "depends": [
+        'base',
+        'account',
+    ],
+    "author": "Coop IT Easy SCRLfs",
+    "category": "Accounting & Finance",
+    "website": "https://coopiteasy.be",
+    "description": """
+        This module requires to check this box to validate the invoice 
+        if invoices with the same partner, invoice date and totam alount already
+        exist.
+    """,
+    'data': [
+        "views/account_invoice.xml",
+    ],
+    'installable': True,
+}
