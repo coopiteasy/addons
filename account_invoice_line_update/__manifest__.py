94d6db0bddc0c12cec5d374a676dd761bbecc7a5
houssine <houssine.bakkali@gmail.com>
2020-06-04 19:56:43 +0200

    [ADD] update invoice lines
    This modules will update the lines. It will first save qty and unit
    amount then call onchange_product and set back qty and unit_amount. This
    is usefull when you want to recompute wrongly settled tax in batch on
    several invoices.

---
+++ account_invoice_line_update/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 - Today Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Houssine Bakkali  <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Invoice lines update",
+    "summary": """
+    Update invoice lines to reload the right taxes on the lines.
+    """,
+    "description": """
+    """,
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "version": "12.0.1.0.0",
+    "website": "https://www.coopiteasy.be",
+    "category": "Accounting",
+    "depends": ["account"],
+    "data": [
+        "wizard/invoice_lines_update.xml",
+    ],
+}
