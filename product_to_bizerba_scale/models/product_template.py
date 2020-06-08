27dd6709d899f34656aef6d5df3ac4b7c170597e
Vincent Van Rossem <vvrossem@gmail.com>
2020-06-08 13:57:36 +0200

    [IMP] product_to_bizerba_scale: defered -> deferred

--- product_to_bizerba_scale/models/product_template.py
+++ product_to_bizerba_scale/models/product_template.py
@@ -106,32 +106,32 @@ class ProductTemplate(models.Model):
 
     @api.multi
     def write(self, vals):
-        defered = {}
+        deferred = {}
         for product in self:
             if product.is_new_in_scale(vals):
                 # Product is new to the scale system: create it.
-                defered[product.id] = 'create'
+                deferred[product.id] = 'create'
             elif product.is_in_scale() and product.will_be_in_scale(vals):
                 # Product is in the scale system and will be in the
                 # scale system after the write: if there is changes in
                 # the fields related to the scale system, update it.
                 if product._check_vals_scale_bizerba(vals):
-                    defered[product.id] = 'write'
+                    deferred[product.id] = 'write'
                 # If scale_group has change, product must be updated.
                 if ('scale_group_id' in vals
                         and vals['scale_group_id'] != product.scale_group_id):
                     # Remove it from previous group
                     product._send_to_scale_bizerba('unlink')
                     # Send it in the new group
-                    defered[product.id] = 'create'
+                    deferred[product.id] = 'create'
             elif product.is_in_scale() and not product.will_be_in_scale(vals):
                 # Product is in the scale system and will no longer be
                 # in the scale system after the write: delete it.
-                defered[product.id] = 'unlink'
+                deferred[product.id] = 'unlink'
 
         res = super(ProductTemplate, self).write(vals)
 
-        for product_id, action in defered.items():
+        for product_id, action in deferred.items():
             product = self.browse(product_id)
             product._send_to_scale_bizerba(action, True)
 
