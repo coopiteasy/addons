9433c9cc19c5ee9d3af72b9d00f4ad5e6e73e3c2
robin.keunen <robin@keunen.net>
2020-06-17 11:03:09 +0200

    [REF] resource_*: blacken

--- resource_planning/models/resource.py
+++ resource_planning/models/resource.py
@@ -7,162 +7,180 @@ from openerp.exceptions import ValidationError, UserError
 
 
 class ResourceCategory(models.Model):
-    _name = 'resource.category'
-    _inherit = 'mail.thread'
+    _name = "resource.category"
+    _inherit = "mail.thread"
 
-    name = fields.Char(
-        string="Category name",
-        required=True,
-        translate=True,
-    )
+    name = fields.Char(string="Category name", required=True, translate=True,)
     resources = fields.One2many(
-        'resource.resource',
-        'category_id',
-        string="Resources")
-    is_accessory = fields.Boolean(
-        string="Is Accessory")
+        "resource.resource", "category_id", string="Resources"
+    )
+    is_accessory = fields.Boolean(string="Is Accessory")
 
     active = fields.Boolean(
-            'Active', default=True,
-            track_visibility='onchange')
+        "Active", default=True, track_visibility="onchange"
+    )
 
 
 class Resource(models.Model):
-    _name = 'resource.resource'
-    _inherit = ['resource.resource', 'mail.thread']
+    _name = "resource.resource"
+    _inherit = ["resource.resource", "mail.thread"]
 
     @api.model
     def _get_default_location(self):
         location = self.env.user.resource_location
         if not location:
-            main_location = self.env.ref('resource_planning.main_location', False)
-            return main_location if main_location else self.env['resource.location']
+            main_location = self.env.ref(
+                "resource_planning.main_location", False
+            )
+            return (
+                main_location
+                if main_location
+                else self.env["resource.location"]
+            )
 
         return location
 
-    category_id = fields.Many2one(
-        'resource.category',
-        string="Category")
+    category_id = fields.Many2one("resource.category", string="Category")
     state = fields.Selection(
-        [('draft', 'Draft'),
-         ('available', 'Available'),
-         ('unavailable', 'Unavailable')],
+        [
+            ("draft", "Draft"),
+            ("available", "Available"),
+            ("unavailable", "Unavailable"),
+        ],
         string="State",
-        default='draft')
+        default="draft",
+    )
     resource_type = fields.Selection(
-        [('user', 'Human'), ('material', 'Material')],
+        [("user", "Human"), ("material", "Material")],
         string="Resource Type",
         required=True,
-        default="material")
+        default="material",
+    )
     allocations = fields.One2many(
-        'resource.allocation',
-        'resource_id',
-        string="Booking lines")
-    serial_number = fields.Char(
-        string="ID number")
+        "resource.allocation", "resource_id", string="Booking lines"
+    )
+    serial_number = fields.Char(string="ID number")
     location = fields.Many2one(
-        'resource.location',
-        string="Location",
-        default=_get_default_location)
+        "resource.location", string="Location", default=_get_default_location
+    )
     removed_from_stock = fields.Boolean(
-        string='Removed From Stock',
-        default=False)
+        string="Removed From Stock", default=False
+    )
     stock_removal_reason = fields.Selection(
-        string='Stock Removal Reason',
-        selection=[('sold', 'Sold'),
-                   ('stolen', 'Stolen'),
-                   ('given', 'Given'),
-                   ('broken', 'Broken'),
-                   ('other', 'Other')])
-    purchase_date = fields.Date(
-        string='Purchase Date')
-    stock_removal_date = fields.Date(
-        string='Stock Removal Date')
-    selling_price = fields.Float(
-        string='Selling Price')
-    purchase_invoice_ref = fields.Char(
-        string='Purchase Invoice Ref')
-    sale_invoice_ref = fields.Char(
-        string='Sale Invoice Ref')
+        string="Stock Removal Reason",
+        selection=[
+            ("sold", "Sold"),
+            ("stolen", "Stolen"),
+            ("given", "Given"),
+            ("broken", "Broken"),
+            ("other", "Other"),
+        ],
+    )
+    purchase_date = fields.Date(string="Purchase Date")
+    stock_removal_date = fields.Date(string="Stock Removal Date")
+    selling_price = fields.Float(string="Selling Price")
+    purchase_invoice_ref = fields.Char(string="Purchase Invoice Ref")
+    sale_invoice_ref = fields.Char(string="Sale Invoice Ref")
 
     _sql_constraints = [
-        ('name_uniq', 'unique (name)',
-         'The name of the resource must be unique !')
+        (
+            "name_uniq",
+            "unique (name)",
+            "The name of the resource must be unique !",
+        )
     ]
 
     @api.multi
     def action_unavailable(self):
         for resource in self:
-            resource.state = 'unavailable'
+            resource.state = "unavailable"
 
     @api.multi
     def action_available(self):
         for resource in self:
-            resource.state = 'available'
+            resource.state = "available"
 
     @api.multi
     def action_draft(self):
         for resource in self:
-            resource.state = 'draft'
+            resource.state = "draft"
 
     def check_dates(self, date_start, date_end):
         if not date_start or not date_end:
-            raise ValidationError(_("Error. Date start or date end aren't set"))
+            raise ValidationError(
+                _("Error. Date start or date end aren't set")
+            )
         elif date_end < date_start:
-            raise ValidationError(_("Error. End date is preceding start "
-                                    "date. Please choose an end date after a "
-                                    "start date "))
+            raise ValidationError(
+                _(
+                    "Error. End date is preceding start "
+                    "date. Please choose an end date after a "
+                    "start date "
+                )
+            )
 
     @api.multi
     def check_availabilities(self, date_start, date_end, location):
         self.check_dates(date_start, date_end)
-        available_resources = self.filtered(lambda r: r.state == 'available')
+        available_resources = self.filtered(lambda r: r.state == "available")
         if location:
             available_resources = available_resources.filtered(
-                lambda r: r.location.id == location.id)
+                lambda r: r.location.id == location.id
+            )
         available_resources_ids = available_resources.ids
 
         # assert start < end
         conflicting_allocation_domain = [
-            ('resource_id', 'in', available_resources_ids),
-            ('state', '!=', 'cancel'),
-            '!',
-                '|', ('date_end', '<=', date_start),
-                     ('date_start', '>=', date_end)
+            ("resource_id", "in", available_resources_ids),
+            ("state", "!=", "cancel"),
+            "!",
+            "|",
+            ("date_end", "<=", date_start),
+            ("date_start", ">=", date_end),
         ]
-        
-        conflicting_allocations = (
-            self.env['resource.allocation']
-                .search(conflicting_allocation_domain)
+
+        conflicting_allocations = self.env["resource.allocation"].search(
+            conflicting_allocation_domain
         )
-        unavailable_resources_ids = conflicting_allocations.mapped('resource_id.id')  # noqa
-        
+        unavailable_resources_ids = conflicting_allocations.mapped(
+            "resource_id.id"
+        )  # noqa
+
         for resource_id in unavailable_resources_ids:
             available_resources_ids.remove(resource_id)
         return available_resources_ids
-    
+
     @api.multi
-    def allocate_resource(self, allocation_type, date_start, date_end,
-                          partner_id, location, date_lock=False):
+    def allocate_resource(
+        self,
+        allocation_type,
+        date_start,
+        date_end,
+        partner_id,
+        location,
+        date_lock=False,
+    ):
         self.check_dates(date_start, date_end)
-        res_alloc = self.env['resource.allocation']
+        res_alloc = self.env["resource.allocation"]
 
         vals = {
-            'date_start': date_start,
-            'date_end': date_end,
-            'date_lock': date_lock,
-            'state': allocation_type,
-            'partner_id': partner_id.id,
-            'location': location.id
+            "date_start": date_start,
+            "date_end": date_end,
+            "date_lock": date_lock,
+            "state": allocation_type,
+            "partner_id": partner_id.id,
+            "location": location.id,
         }
 
-        # we check again the availabilities in case in has been booked 
-        # between the search and the allocation request  
+        # we check again the availabilities in case in has been booked
+        # between the search and the allocation request
         allocation_ids = []
-        available_resource_ids = self.check_availabilities(date_start, date_end, location)
+        available_resource_ids = self.check_availabilities(
+            date_start, date_end, location
+        )
 
         for resource in self.browse(available_resource_ids):
-            vals['resource_id'] = resource.id
+            vals["resource_id"] = resource.id
             allocation = res_alloc.create(vals)
             allocation_ids.append(allocation.id)
         return allocation_ids
@@ -170,55 +188,52 @@ class Resource(models.Model):
 
 class ResourceAllocation(models.Model):
     _name = "resource.allocation"
-    
+
     # _inherit = ['mail.thread']
-    
-    name = fields.Many2one(
-        related="partner_id")
+
+    name = fields.Many2one(related="partner_id")
     serial_number = fields.Char(
-        related="resource_id.serial_number",
-        string="Serial number")
+        related="resource_id.serial_number", string="Serial number"
+    )
     resource_id = fields.Many2one(
-        'resource.resource',
-        string="Resource",
-        required=True)
+        "resource.resource", string="Resource", required=True
+    )
     resource_category_id = fields.Many2one(
-        related='resource_id.category_id',
+        related="resource_id.category_id",
         string="Resource Category",
-        store=True)
-    date_start = fields.Datetime(
-        string="Date start")
-    date_end = fields.Datetime(
-        string="Date end")
+        store=True,
+    )
+    date_start = fields.Datetime(string="Date start")
+    date_end = fields.Datetime(string="Date end")
     state = fields.Selection(
-        [('booked', 'Booked'),
-         ('option', 'Option'),
-         ('maintenance', 'Maintenance'),
-         ('cancel', 'Cancel')],
-         string="State",
-        default='option')
+        [
+            ("booked", "Booked"),
+            ("option", "Option"),
+            ("maintenance", "Maintenance"),
+            ("cancel", "Cancel"),
+        ],
+        string="State",
+        default="option",
+    )
     date_lock = fields.Date(
         string="Lock date",
         help="If the booking type is option, it should be confirmed before "
-             "the lock date expire")
-    partner_id = fields.Many2one(
-        'res.partner',
-        string="Partner")
-    location = fields.Many2one(
-        'resource.location',
-        string="Location")
+        "the lock date expire",
+    )
+    partner_id = fields.Many2one("res.partner", string="Partner")
+    location = fields.Many2one("resource.location", string="Location")
 
     @api.multi
     def action_confirm(self):
         for allocation in self:
-            allocation.write({'state': 'booked', 'date_lock': None})
+            allocation.write({"state": "booked", "date_lock": None})
 
     @api.multi
     def action_cancel(self):
         for allocation in self:
-            allocation.state = 'cancel'
+            allocation.state = "cancel"
 
     @api.multi
     def action_option(self):
         for allocation in self:
-            allocation.state = 'option'
+            allocation.state = "option"
--- resource_planning/models/resource_location.py
+++ resource_planning/models/resource_location.py
@@ -6,73 +6,69 @@ from openerp import _, api, fields, models
 
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
-    resource_location = fields.Many2one(
-        'resource.location',
-        string="Location")
+    resource_location = fields.Many2one("resource.location", string="Location")
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     @api.model
     def _get_default_location(self):
         location = self.env.user.resource_location
         if not location:
-            main_location = self.env.ref('resource_planning.main_location', False)
-            return main_location if main_location else self.env['resource.location']
+            main_location = self.env.ref(
+                "resource_planning.main_location", False
+            )
+            return (
+                main_location
+                if main_location
+                else self.env["resource.location"]
+            )
         return location
 
     resource_location = fields.Many2one(
-        'resource.location',
+        "resource.location",
         string="Location",
         default=_get_default_location,
-        domain=[('main_location', '=', True)],
+        domain=[("main_location", "=", True)],
     )
 
 
 class ResourceLocation(models.Model):
-    _name = 'resource.location'
-    _inherit = 'mail.thread'
+    _name = "resource.location"
+    _inherit = "mail.thread"
 
     @api.multi
-    @api.depends('resources')
+    @api.depends("resources")
     def _compute_available_resources(self):
         for location in self:
-            resources = (
-                self.env['resource.resource']
-                    .search([('location', '=', location.id)])
+            resources = self.env["resource.resource"].search(
+                [("location", "=", location.id)]
             )
-            location.resource_categories = resources.mapped('category_id')
+            location.resource_categories = resources.mapped("category_id")
         return True
 
-    name = fields.Char(
-        string="Name")
-    main_location = fields.Boolean(
-        default=False)
-    address = fields.Many2one(
-        'res.partner',
-        string="Address")
+    name = fields.Char(string="Name")
+    main_location = fields.Boolean(default=False)
+    address = fields.Many2one("res.partner", string="Address")
     customers = fields.One2many(
-        'res.partner',
-        'resource_location',
-        domain=[('customer', '=', True)],
-        string="Customers")
+        "res.partner",
+        "resource_location",
+        domain=[("customer", "=", True)],
+        string="Customers",
+    )
     resources = fields.One2many(
-        'resource.resource',
-        'location',
-        string="Resources")
-    users = fields.One2many(
-        'res.users',
-        'resource_location',
-        string="Users")
+        "resource.resource", "location", string="Resources"
+    )
+    users = fields.One2many("res.users", "resource_location", string="Users")
     resource_categories = fields.Many2many(
-        comodel_name='resource.category',
-        string='Available Categories',
+        comodel_name="resource.category",
+        string="Available Categories",
         compute=_compute_available_resources,
-        store=True)
+        store=True,
+    )
     active = fields.Boolean(
-            "Active",
-            default=True,
-            track_visibility="onchange")
+        "Active", default=True, track_visibility="onchange"
+    )
