bee77b49b097c77fb4ce72a3cf59f5bd98b9ed15
robin.keunen <robin@keunen.net>
2020-09-11 18:32:05 +0200

    [IMP] cie_custom: order account by frequency
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- coopiteasy_custom/models/__init__.py
+++ coopiteasy_custom/models/__init__.py
@@ -1,2 +1,3 @@
+from . import analytic_account
 from . import sale_timesheet
 from . import task
---
+++ coopiteasy_custom/models/analytic_account.py
@@ -0,0 +1,26 @@
+# Copyright 2020 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+from datetime import date, timedelta
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = "account.analytic.account"
+    _order = "line_count desc"
+
+    line_count = fields.Integer(
+        string="Line Count", compute="_compute_line_count", store=True
+    )
+
+    @api.depends("line_ids", "account_type")
+    @api.multi
+    def _compute_line_count(self):
+        # may lead to performance issues. See with time.
+        for account in self:
+            one_month_ago = date.today() - timedelta(days=30)
+            recent_lines = account.line_ids.filtered(
+                lambda l: fields.Date.from_string(l.date) >= one_month_ago
+            )
+            account.line_count = len(recent_lines)
