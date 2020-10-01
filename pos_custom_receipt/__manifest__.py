848f95d3ee8428dae62542abb9993ff77254e28b
Vincent Van Rossem <vvrossem@gmail.com>
2020-10-01 11:15:24 +0200

    [12.0][IMP] pos_custom_receipt: module description / readme / black

--- pos_custom_receipt/__manifest__.py
+++ pos_custom_receipt/__manifest__.py
@@ -4,18 +4,17 @@
 {
     "name": "POS Custom Receipt",
     "version": "12.0.1.0.0",
-    "depends": [
-        "point_of_sale",
-    ],
+    "depends": ["point_of_sale",],
     "author": "Coop IT Easy SCRLfs",
     "category": "Point Of Sale",
     "website": "https://coopiteasy.be",
     "license": "AGPL-3",
     "description": """
-    This module extends the pos receipt in order to hide some info and
-    adds other info.
+    This module extends the POS receipt to:
+    - Hide company's email
+    - Add customer's name
     """,
-    'data': [],
-    'qweb': ['static/src/xml/pos.xml'],
-    'installable': True,
+    "data": [],
+    "qweb": ["static/src/xml/pos.xml"],
+    "installable": True,
 }
---
+++ pos_custom_receipt/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module extends the POS receipt to:
+
+* Hide company's email
+* Add customer's name
