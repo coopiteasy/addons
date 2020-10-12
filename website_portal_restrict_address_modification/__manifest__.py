6871c3b8ce1cd9f745fb1ec916c2e99de4f18a3b
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-10-12 12:00:33 +0200

    [ADD] website_postal_restrict_address_modification

---
+++ website_portal_restrict_address_modification/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Website Portal Restrict Address Modification",
+    "version": "11.0.1.0.0",
+    "depends": ["portal", "website"],
+    "summary": """
+    Portal extension preventing modification of the address fields by the users
+    """,
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "website": "https://coopiteasy.be/",
+    "category": "Portal",
+    "data": ["views/portal_website_templates.xml"],
+    "installable": True,
+}
---
+++ website_portal_restrict_address_modification/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Rémy TAYMANS <remy@coopiteasy.be>
+* Vincent VAN ROSSEM <vincent@coopiteasy.be>
+* Manuel CLAEYS BOUUAERT <manuel@coopiteasy.be>
