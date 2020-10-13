738d0e75649e3fa821751e05cd9ac43a1c8f8c21
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-10-13 12:00:45 +0200

    [MIG] account_invoice_structured_communication: rename account_invoice_provider_reference

---
+++ account_invoice_provider_reference/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Coop IT Easy SCRLfs
+# Nicolas Jamoulle, <nicolas@coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Structured Communication",
+    "version": "12.0.1.0.0",
+    "author": "Coop IT Easy SCRLfs",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "website": "https://www.coopiteasy.be",
+    "depends": ["account"],
+    "data": ["views/account_invoice_view.xml"],
+    "installable": True,
+}
