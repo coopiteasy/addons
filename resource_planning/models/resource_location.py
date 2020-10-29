2620751b416d3bf39c640536c153426a657254e4
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-10-29 15:57:39 +0100

    [ADD] resource_planning: partner_bank_id

--- resource_planning/models/resource_location.py
+++ resource_planning/models/resource_location.py
@@ -53,6 +53,9 @@ class ResourceLocation(models.Model):
     name = fields.Char(string="Name")
     main_location = fields.Boolean(default=False)
     address = fields.Many2one("res.partner", string="Address")
+    partner_bank_id = fields.Many2one(
+        'res.partner.bank',
+        string='Bank Account')
     customers = fields.One2many(
         "res.partner",
         "resource_location",
