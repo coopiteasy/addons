b9bcbdecddb58d453f2aa3f35cc87e06ceda4328
Rémy Taymans <remytms@tsmail.eu>
2021-01-05 16:20:23 +0100

    [ADD] stock_inventory_confirm_reset_qty

---
+++ stock_inventory_confirm_reset_qty/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Coop IT Easy SCRLfs (https://coopiteasy.be)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Stock Inventory Confirm Reset Product Qty',
+    'description': """
+        Show a confirmation dialog box when clicking on
+        'Set quantities to 0' in a stock.inventory""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Coop IT Easy SCRLfs',
+    'website': 'https://github.com/coopiteasy/addons',
+    'depends': [
+        "stock",
+    ],
+    'data': [
+        "views/stock_inventory_views.xml",
+    ],
+    'demo': [
+    ],
+}
