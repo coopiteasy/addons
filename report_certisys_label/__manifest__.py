5ce2998e654da58188f4c64aec0729a3d701dd95
Vincent Van Rossem <vvrossem@gmail.com>
2021-04-27 11:15:46 +0200

    [12.0][REF] account_invoice_label_certisys >> report_certisys_label

--- report_certisys_label/__manifest__.py
+++ report_certisys_label/__manifest__.py
@@ -3,7 +3,7 @@
 #     - Manuel Claeys Bouuaert <manuel@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Account Invoice Label Certisys",
+    "name": "Report Certisys Label",
     "summary": "Add Certisys Label on account, stock and sale reports",
     "category": "Reporting",
     "version": "12.0.1.0.0",
