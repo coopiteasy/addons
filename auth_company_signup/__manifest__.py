25cdbf778274ed7b12443c40ebd604dd6e8de5d4
houssine <houssine.bakkali@gmail.com>
2021-04-28 15:55:26 +0200

    [ADD] add module auth_company_signup

---
+++ auth_company_signup/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 - Today Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Houssine Bakkali <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Company Signup",
+    "summary": """
+        This module allows a user to sign up as a company.
+    """,
+    "version": "12.0.1.0.0",
+    "depends": [
+        "auth_signup",
+    ],
+    "author": "Coop IT Easy SCRLfs",
+    "website": "https://www.coopiteasy.be",
+    "data": [
+        "views/auth_signup_assets.xml",
+        "views/auth_signup_template.xml"
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
---
+++ auth_company_signup/static/src/js/auth_signup.js
@@ -0,0 +1,15 @@
+odoo.define('auth_company_signup.auth_signup', function (require) {
+	"use strict";
+
+	$(document).ready(function () {
+		$("input[name='is_company']").click(function(ev) {
+			var val = $("input[name='is_company']:checked").val();
+			console.log(val);
+			if(val == "on") {
+				$("div[name='vat_div']").show('quick');
+			} else {
+				$("div[name='vat_div']").hide('quick');
+			}
+		});
+	});
+});
