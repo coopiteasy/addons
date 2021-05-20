cdc35b32ff76ceebfccafd3b590bdd18be717a43
greg <gregoire@coopiteasy.be>
2021-05-20 11:39:59 +0200

    [ADD]: account_invoice_save_check_duplicate: create module to check that account invoice isn't a duplicate

---
+++ account_invoice_save_check_duplicate/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 - Today Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Grégoire Leeuwerck  <gregoire@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account invoice check duplicate on save",
+    "summary": """
+    Check that account invoice hasn't been encoded twice when creating or saving. This step is normally done when validating. This step doesn't replace the validation.
+    """,
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "version": "12.0.1.0.0",
+    "website": "https://www.coopiteasy.be",
+    "category": "Accounting",
+    "depends": ["account"],
+}
---
+++ account_invoice_save_check_duplicate/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+    Adds a check that account invoice hasn't been encoded twice when creating or saving it. This step is normally done when validating. This step doesn't replace the validation.
