4e0fbedeb47099d9fb597c75c364ac55375c278c
hugues de keyzer <odoo@hugues.info>
2021-10-20 11:22:00 +0200

    [ADD] new module resource_work_time_from_contracts

---
+++ resource_work_time_from_contracts/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Resource Work Time From Contracts",
+    "summary": (
+        "Take the contracts of an employee into account when computing work "
+        "time per day"
+    ),
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Coop IT Easy SCRLfs",
+    "website": "https://coopiteasy.be",
+    "depends": [
+        "hr_contract",
+    ],
+    "data": [],
+    "demo": [],
+}
---
+++ resource_work_time_from_contracts/readme/DESCRIPTION.rst
@@ -0,0 +1,9 @@
+Take the contracts of an employee into account when computing work time per
+day.
+
+When this module is installed, the number of hours an employee is supposed to
+work is only computed from their contracts. Without contracts, the work time
+per day is 0, instead of using the default company’s work schedule.
+
+The start and end dates of contracts are taken into account, but the status
+(state) of the contract is ignored.
