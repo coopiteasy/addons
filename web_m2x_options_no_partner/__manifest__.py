5b62aeab1302dab39a4365471c6a4630160a4947
Carmen Bianca Bakker <carmen@coopiteasy.be>
2021-12-07 14:11:27 +0100

    [ADD] web_m2x_options_no_partner
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ web_m2x_options_no_partner/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2021 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "web_m2x_options_no_partner",
+    "description": """
+        Removes creation options from (some) partner dropdown menus.""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Coop IT Easy SCRLfs",
+    "website": "https://coopiteasy.be",
+    "depends": [
+        "web_m2x_options",
+        "account",
+        "purchase",
+        "sale",
+        "stock",
+    ],
+    "data": [
+        "views/account.xml",
+        "views/purchase.xml",
+        "views/sale.xml",
+        "views/stock.xml",
+    ],
+    "demo": [],
+}
