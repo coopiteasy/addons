9c7e12bba0730ebb4a078dbb208e8ef4b4137223
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-01-07 18:01:45 +0100

    [ADD] stock_picking_only_suppliers_products
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ stock_picking_only_suppliers_products/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking Only Supplier's Products",
+    "summary": """
+        On a stock picking, only display the supplier's products.""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Coop IT Easy SCRLfs",
+    "website": "https://coopiteasy.be",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_picking.xml",
+    ],
+    "demo": [],
+}
