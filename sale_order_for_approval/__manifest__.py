7a8de217378383a202ce6203d39c603fe7a7fa80
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-01-10 10:06:09 +0100

    [10.0][MIG] sale_order_for_approval: Migration to 10.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ sale_order_for_approval/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2017- Robin Keunen - Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Order - For Approval Mention",
+    "version": "10.0.1.0.0",
+    "category": "Sales",
+    "sequence": 95,
+    "author": "Robin KEUNEN- Coop IT Easy SCRLfs",
+    "summary": 'Display "For Approval" mention on Sale Orders',
+    "description": """
+
+============================
+
+This module adds a "For approval section to sale orders"
+
+    """,
+    "depends": ["sale"],
+    "data": [
+        "views/report_saleorder.xml",
+    ],
+    "installable": True,
+}
