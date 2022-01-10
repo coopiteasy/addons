82f3bd37822a04eabe586acbcdc403de3a3e0c1f
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-01-10 10:08:20 +0100

    [11.0][MIG] sale_order_for_approval: Migration to 11.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- sale_order_for_approval/__manifest__.py
+++ sale_order_for_approval/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2017- Robin Keunen - Coop IT Easy SCRLfs
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Sale Order - For Approval Mention",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Sales",
     "sequence": 95,
     "author": "Robin KEUNEN- Coop IT Easy SCRLfs",
