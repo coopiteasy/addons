0b4e6812776b1948ed17388ba70c67a6b0a9a65d
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-01-10 14:16:22 +0100

    [IMP] stock_picking_mail_incorrect_qty: Include upstream code to prevent bug
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- stock_picking_mail_incorrect_qty/models/stock_picking.py
+++ stock_picking_mail_incorrect_qty/models/stock_picking.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
+from odoo.tools.translate import _
 
 
 class Picking(models.Model):
@@ -62,6 +63,61 @@ class Picking(models.Model):
 
     @api.multi
     def action_done(self):
+        # The following code between the 'fmt' tags is copied almost-verbatim
+        # from the stock module. It handles a corner case of move.lines being
+        # unlinked to moves, but linked to stock.picking. It is copied here
+        # verbatim because not including it would (potentially) result in some
+        # moves being skipped over.
+        #
+        # The order of operations we want:
+        #
+        # - Copied code snippet below from stock_picking.action_done(). This
+        #   fixes discrepancies as described.
+        # - Send e-mail
+        # - The remainder of stock_picking.action_done(). This function
+        #   'destroys' the information that is needed for composing the e-mail
+        #   (e.g., empty moves are cancelled, quantities are split and adjusted,
+        #   etc etc etc).
+        #
+        # If we do the following order:
+        #
+        # - Send e-mail
+        # - All of stock_picking.action_done()
+        #
+        # ... then the e-mailing functionality may not have access to the fixed
+        # discrepancies.
+        #
+        # Further discussed and described in
+        # <https://github.com/coopiteasy/addons/pull/192>.
+
+        # flake8: noqa
+        # fmt: off
+        # Check if there are ops not linked to moves yet
+        for pick in self:
+            # # Link existing moves or add moves when no one is related
+            for ops in pick.move_line_ids.filtered(lambda x: not x.move_id):
+                # Search move with this product
+                moves = pick.move_lines.filtered(lambda x: x.product_id == ops.product_id)
+                moves = sorted(moves, key=lambda m: m.quantity_done < m.product_qty, reverse=True)
+                if moves:
+                    ops.move_id = moves[0].id
+                else:
+                    new_move = self.env['stock.move'].create({
+                                                    'name': _('New Move:') + ops.product_id.display_name,
+                                                    'product_id': ops.product_id.id,
+                                                    'product_uom_qty': ops.qty_done,
+                                                    'product_uom': ops.product_uom_id.id,
+                                                    'location_id': pick.location_id.id,
+                                                    'location_dest_id': pick.location_dest_id.id,
+                                                    'picking_id': pick.id,
+                                                    'picking_type_id': pick.picking_type_id.id,
+                                                   })
+                    ops.move_id = new_move.id
+                    new_move = new_move._action_confirm()
+                    # todo_moves |= new_move
+                    #'qty_done': ops.qty_done})
+        # fmt: on
+
         self._notify_incorrect_delivery()
 
         return super(Picking, self).action_done()
