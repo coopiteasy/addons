9be7b27e1679afcdfcf32c5767ac4d203ff8642d
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-01-12 15:39:43 +0100

    [FIX] sale_order_for_approval: Adjust fields in manifest
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- sale_order_for_approval/__manifest__.py
+++ sale_order_for_approval/__manifest__.py
@@ -5,13 +5,12 @@
     "name": "Sale Order - For Approval Mention",
     "version": "12.0.1.0.0",
     "category": "Sales",
-    "sequence": 95,
     "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
     "website": "https://coopiteasy.be",
     "summary": 'Display "For Approval" mention on Sale Orders',
     "depends": ["sale"],
     "data": [
         "views/report_saleorder.xml",
     ],
-    "installable": True,
 }
