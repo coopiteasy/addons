668556e4447db26fd701945cebeb2910c27ad006
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-01-17 18:00:28 +0100

    [ADD] account_missing_tax
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ vracoop-modules/account_missing_tax/models/res_company.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class Company(models.Model):
+    _inherit = "res.company"
+
+    fallback_account_id = fields.Many2one(
+        comodel_name="account.account",
+        string="Fallback Account",
+        help="Moves for which the tax was undefined will be assigned to this account.",
+        required=True,
+    )
---
+++ vracoop-modules/account_missing_tax/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Define an account for when the tax is unassigned, for each company.
