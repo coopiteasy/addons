2939b2d531732b1c56aa81ac84c9d1d2469d85c8
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-02-11 13:10:02 +0100

    [REF] cookingo_custom: Move the automatic computation to /shop/payment
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ cookingo_custom/controllers/main.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Coop IT Easy SCRL fs
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSaleDelivery(WebsiteSale):
+
+    @http.route(['/shop/payment'], type='http', auth="public", website=True)
+    def payment(self, **post):
+        order = request.website.sale_get_order()
+        order.add_containers()
+
+        return super().payment(**post)
