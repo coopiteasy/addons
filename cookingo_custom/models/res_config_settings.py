e217606003d6e0fb966104a22e8f06a71d67c786
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-02-16 14:11:50 +0100

    [FIX] cookingo_custom: Run pre-commit
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- cookingo_custom/models/res_config_settings.py
+++ cookingo_custom/models/res_config_settings.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Coop IT Easy SCRL fs
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
