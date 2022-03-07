13cb362e9aa04c34c0248c00b5efe394320b2b91
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-03-07 15:33:25 +0100

    [MOV] cookingo_custom → container_meals
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ container_meals/models/ir_config_parameter.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Coop IT Easy SCRL fs
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class ConfigParameter(models.Model):
+    _inherit = "ir.config_parameter"
+
+    @api.model
+    def get_container_deposit_product_id(self):
+        deposit_product_id = self.sudo().get_param(
+            "container_meals.container_deposit_product_id"
+        )
+        return self.env["product.product"].browse(int(deposit_product_id))
---
+++ container_meals/tests/test_product.py
@@ -0,0 +1,32 @@
+# Copyright 2022 Coop IT Easy SCRL fs
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import common
+
+
+class TestProduct(common.TestCommon):
+    def test_container_volume_smaller_for_children(self):
+        """A child portion's container volume is 2/3 as big as an
+        adult portion.
+        """
+        self.assertAlmostEqual(
+            self.salad_template.container_1_volume * (2 / 3),
+            self.salad_product_child.container_1_volume,
+        )
+        self.assertAlmostEqual(
+            self.salad_template.container_2_volume * (2 / 3),
+            self.salad_product_child.container_2_volume,
+        )
+
+    def test_container_volume_identical_for_adults(self):
+        """Adult portions are equally as big as declared in the
+        template.
+        """
+        self.assertAlmostEqual(
+            self.salad_template.container_1_volume,
+            self.salad_product_adult.container_1_volume,
+        )
+        self.assertAlmostEqual(
+            self.salad_template.container_2_volume,
+            self.salad_product_adult.container_2_volume,
+        )
