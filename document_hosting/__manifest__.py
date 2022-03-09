929e2c672e07b90eb61854a06b133a2d23c78742
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-03-09 17:31:54 +0100

    [FIX] Run pre-commit
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- document_hosting/__manifest__.py
+++ document_hosting/__manifest__.py
@@ -11,7 +11,7 @@
     "author": "Coop IT Easy SCRLfs",
     "license": "AGPL-3",
     "version": "12.0.1.0.0",
-    "website": "www.coopiteasy.be",
+    "website": "https://coopiteasy.be",
     "category": "Document",
     "depends": ["base", "web", "website", "mail"],
     "data": [
