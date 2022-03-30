e6ef631f80c2076ea62c87d9d51d38fa2df2c1b0
Rémy Taymans <remytms@tsmail.eu>
2022-03-30 16:42:53 +0200

    [ADD] delivery_product_restriction

---
+++ delivery_product_restriction/models/product_template.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = "product.template"
+
+    restrict_delivery_carrier_to = fields.Many2many(
+        comodel_name="delivery.carrier",
+        string="Restrict Delivery Carrier To",
+        help="This product can only be shipped with the following "
+        "delivery carrier. Left empty to allow all delivery carrier to "
+        "be used.",
+    )
---
+++ delivery_product_restriction/readme/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+It allows some product to be shipped only by some delivery carrier. If
+such a product is found in a SO then the SO can only be shipped via
+these delivery carrier.
+
+To get this features works in the eCommerce install the
+`website_sale_delivery_product_restriction` module.
