30178eca79a8983599189bed55e9fc476c906584
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-04-14 12:40:35 +0200

    [FIX] stock_picking_mail_incorrect_qty: Run pre-commit
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- stock_picking_mail_incorrect_qty/__manifest__.py
+++ stock_picking_mail_incorrect_qty/__manifest__.py
@@ -4,7 +4,8 @@
 {
     "name": "Stock Picking Mail Incorrect Quantity",
     "description": """
-        Send an e-mail about incorrect amounts received when confirming a stock picking.""",
+        Send an e-mail about incorrect amounts received when confirming a stock
+        picking.""",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Coop IT Easy SCRLfs",
