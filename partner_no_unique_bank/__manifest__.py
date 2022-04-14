d397dfd133b71bbe8e91164678d4af2b9cb49a11
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-04-14 16:22:56 +0200

    [ADD] partner_no_unique_bank
    Taken from easy_my_coop/cooperator and put into its own module.
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ partner_no_unique_bank/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 Coop IT Easy SC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner No Unique Bank",
+    "summary": """
+        Do not mandate that bank account numbers be unique between partners.""",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SC",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "base",
+    ],
+    "excludes": [],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
