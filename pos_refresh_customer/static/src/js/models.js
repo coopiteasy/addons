350c0e5f02602cc2b854104c592ca587710b03b3
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-04-21 16:13:20 +0200

    fixup! [ADD] pos_refresh_customer

--- pos_refresh_customer/static/src/js/models.js
+++ pos_refresh_customer/static/src/js/models.js
@@ -24,6 +24,17 @@ odoo.define("pos_customer_wallet.screens", function (require) {
                 }
             ).then(
                 function (partners) {
+                    // Context: `add_partners` fetches partner models from the
+                    // Odoo database and inserts those models into its own db.
+                    // As an efficiency measure, it does not insert partner
+                    // models if their `write_date` is LESS recent than the last
+                    // time it recorded that a partner model was successfully
+                    // added to the pos db. Computed fields do not normally
+                    // increase `write_date`. Therefore, to make sure that
+                    // computed fields also get updated, just reset the cached
+                    // date to the Unix epoch.
+                    self.db.partner_write_date = "1970-01-01 00:00:00";
+
                     if (self.db.add_partners(partners)) {
                         // check if the partners we got were real updates
                         def.resolve();
