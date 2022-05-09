684c4e28731bfecbb1e49ed6030d5f3755507b37
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-05-09 10:33:40 +0200

    [UPD] container_meals: Bump version
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- container_meals/__manifest__.py
+++ container_meals/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Container Meals",
     "summary": """Deliver meals in containers.""",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Uncategorized",
     "website": "https://coopiteasy.be",
     "author": "Coop IT Easy SCRLfs",
