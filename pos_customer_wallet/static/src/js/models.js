e2b9028206b6b278c24bec82f8b40435fe4d837e
Rémy Taymans <remytms@tsmail.eu>
2022-05-25 17:54:54 +0200

    [ADD] pos_customer_wallet: Show balance on ticket

--- pos_customer_wallet/static/src/js/models.js
+++ pos_customer_wallet/static/src/js/models.js
@@ -5,4 +5,16 @@ odoo.define("pos_customer_wallet.models", function (require) {
 
     models.load_fields("res.partner", ["customer_wallet_balance"]);
     models.load_fields("account.journal", ["is_customer_wallet_journal"]);
+
+    var order_prototype = models.Order.prototype;
+    models.Order = models.Order.extend({
+        export_for_printing: function () {
+            var receipt = order_prototype.export_for_printing.apply(this);
+            var client = this.get("client");
+            receipt.customer_wallet_balance = client
+                ? client.customer_wallet_balance
+                : null;
+            return receipt;
+        },
+    });
 });
