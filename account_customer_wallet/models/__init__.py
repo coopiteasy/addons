772830bd62cfb3e41fbe9578de2d39063523e71a
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-13 11:49:39 +0200

    [REM] account_customer_wallet, pos_customer_wallet
    This is being moved to OCA/account-payment.
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- account_customer_wallet/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import account_journal
-from . import res_company
-from . import res_config_settings
-from . import res_partner
--- pos_customer_wallet/static/src/js/models.js
+++
@@ -1,8 +0,0 @@
-odoo.define("pos_customer_wallet.models", function (require) {
-    "use strict";
-
-    var models = require("point_of_sale.models");
-
-    models.load_fields("res.partner", ["customer_wallet_balance"]);
-    models.load_fields("account.journal", ["is_customer_wallet_journal"]);
-});
