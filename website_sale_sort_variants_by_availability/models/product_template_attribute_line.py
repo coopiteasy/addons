f999a80629c1acada7fe781cbb3f6b6528a1e8e0
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-13 10:58:59 +0200

    [FIX] website_sale_sort_variants_by_availability: Permissions error
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- website_sale_sort_variants_by_availability/models/product_template_attribute_line.py
+++ website_sale_sort_variants_by_availability/models/product_template_attribute_line.py
@@ -12,13 +12,14 @@ class ProductTemplateAttributeLine(models.Model):
         """This is used by `sale.variants` (regular view of product)."""
         super()._set_product_template_value_ids()
         for rec in self:
-            rec.product_template_value_ids = rec.product_template_value_ids.sorted(
+            rec.product_template_value_ids = rec.product_template_value_ids.sudo().sorted(  # noqa
                 # Get product that has product_attribute_value_id (can be multiple).
                 # If multiple, get the one with the highest qty_available.
                 # Reverse sort on that.
                 lambda ptvi: next(
                     iter(
                         self.env["product.product"]
+                        .sudo()
                         .search(
                             [
                                 ("product_tmpl_id", "=", rec.product_tmpl_id.id),
