a246fff2bb389ca9d5356ef6489537c65eb405c8
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-15 18:03:00 +0200

    [ADD] pos_customer_wallet_is_user
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ pos_customer_wallet_partner_is_user/__manifest__.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2022 Coop IT Easy SCRLfs
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Point of Sale Customer Wallet Partner Is User",
+    "summary": """
+        Add a field on partners that shows whether they have used customer wallet
+        functionality, and don't show some parts of customer wallet functionality
+        to partners who haven't already used it.""",
+    "version": "12.0.1.0.0",
+    "category": "Point of Sale",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SCRLfs",
+    "maintainer": "Coop IT Easy SCRLfs",
+    "maintainers": ["coopiteasy"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "pos_customer_wallet",
+    ],
+    "excludes": [],
+    "data": [
+        "templates/assets.xml",
+        "views/res_partner_views.xml",
+    ],
+    "demo": [],
+    "qweb": [
+        "static/src/xml/pos.xml",
+    ],
+}
---
+++ pos_customer_wallet_partner_is_user/static/src/js/models.js
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: 2022 Coop IT Easy SCRLfs
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+odoo.define("pos_customer_wallet_partner_is_user.models", function (require) {
+    "use strict";
+
+    var models = require("point_of_sale.models");
+
+    models.load_fields("res.partner", ["is_customer_wallet_user"]);
+
+    var order_prototype = models.Order.prototype;
+    models.Order = models.Order.extend({
+        export_for_printing: function () {
+            var receipt = order_prototype.export_for_printing.apply(this);
+            var client = this.get("client");
+            receipt.is_customer_wallet_user = client
+                ? client.is_customer_wallet_user
+                : null;
+            return receipt;
+        },
+    });
+});
