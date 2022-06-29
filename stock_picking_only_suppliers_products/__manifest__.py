d683d664fa7f47d943c21bfba4d2829340b9b98d
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-29 11:24:14 +0200

    [FIX] SCRLfs → SC
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- stock_picking_only_suppliers_products/__manifest__.py
+++ stock_picking_only_suppliers_products/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Coop IT Easy SCRLfs
+# Copyright 2022 Coop IT Easy SC
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -7,7 +7,7 @@
         On a stock picking, only display the supplier's products.""",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Coop IT Easy SCRLfs",
+    "author": "Coop IT Easy SC",
     "website": "https://coopiteasy.be",
     "depends": [
         "stock",
