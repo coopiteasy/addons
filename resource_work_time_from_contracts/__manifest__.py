beee10c38b8d17af2f727252e36f4ad5ecd7a206
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-29 11:24:14 +0200

    [FIX] SCRLfs → SC
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- resource_work_time_from_contracts/__manifest__.py
+++ resource_work_time_from_contracts/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Coop IT Easy SCRLfs
+# Copyright 2021 Coop IT Easy SC
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -9,7 +9,7 @@
     ),
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Coop IT Easy SCRLfs",
+    "author": "Coop IT Easy SC",
     "website": "https://coopiteasy.be",
     "category": "Human Resources",
     "depends": [
--- resource_work_time_from_contracts/tests/test_work_days_data.py
+++ resource_work_time_from_contracts/tests/test_work_days_data.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Coop IT Easy SCRLfs
+# Copyright 2021 Coop IT Easy SC
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from datetime import timedelta, timezone
