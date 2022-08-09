e219d4456f2458e1206d2ce78a1ef95aac0d182d
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-08-09 14:28:22 +0200

    [REF] pos_customer_wallet : refactor code and create a sub function to compute wallet for bank statement lines
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- pos_customer_wallet/models/res_partner.py
+++ pos_customer_wallet/models/res_partner.py
@@ -1,25 +1,15 @@
 # Copyright 2022 Coop IT Easy SC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import api, models
 
 
 class Partner(models.Model):
     _inherit = "res.partner"
 
-    def _compute_customer_wallet_balance(self):
-        super()._compute_customer_wallet_balance()
-
+    @api.model
+    def get_wallet_balance_bank_statement_line(self, all_partner_ids):
         account_bank_statement_line = self.env["account.bank.statement.line"].sudo()
-        if not self.ids:
-            return True
-
-        all_partner_families = {}
-        all_partner_ids = set()
-        for partner in self:
-            all_partner_families[partner] = partner.get_all_partners_in_family()
-            all_partner_ids |= set(all_partner_families[partner])
-
         # generate where clause to include multicompany rules
         where_query = account_bank_statement_line._where_calc(
             [
@@ -42,8 +32,26 @@ class Partner(models.Model):
             % where_clause
         )
         self.env.cr.execute(query, where_clause_params)
-        totals = self.env.cr.dictfetchall()
+        return self.env.cr.dictfetchall()
+
+    def _compute_customer_wallet_balance(self):
+        super()._compute_customer_wallet_balance()
+
+        if not self.ids:
+            return True
+
+        all_partner_families = {}
+        all_partner_ids = set()
+        for partner in self:
+            all_partner_families[partner] = partner.get_all_partners_in_family()
+            all_partner_ids |= set(all_partner_families[partner])
+
+        statement_line_totals = self.get_wallet_balance_bank_statement_line(
+            all_partner_ids
+        )
         for partner, child_ids in all_partner_families.items():
             partner.customer_wallet_balance += sum(
-                -total["total"] for total in totals if total["partner_id"] in child_ids
+                -total["total"]
+                for total in statement_line_totals
+                if total["partner_id"] in child_ids
             )
