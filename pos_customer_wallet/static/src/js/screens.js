089bc0fda7212d8d79ba4dfdce4631bb1b5934a8
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-08-09 11:47:07 +0200

    [REF] move overload function at the beginning of the javascript file, and add function comment
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- pos_customer_wallet/static/src/js/screens.js
+++ pos_customer_wallet/static/src/js/screens.js
@@ -6,6 +6,11 @@ odoo.define("pos_customer_wallet.screens", function (require) {
     var _t = core._t;
 
     screens.PaymentScreenWidget.include({
+        /**
+         * Overload function.
+         *
+         * Update balance wallet amount when customer changed.
+         */
         customer_changed: function () {
             this._super();
             if (this.pos.config.is_enabled_customer_wallet) {
@@ -18,27 +23,13 @@ odoo.define("pos_customer_wallet.screens", function (require) {
                 );
             }
         },
-        find_customer_wallet_payment_method() {
-            // This is fairly naive.
-            for (var i = 0; i < this.pos.cashregisters.length; i++) {
-                if (this.pos.cashregisters[i].journal.is_customer_wallet_journal) {
-                    return this.pos.cashregisters[i];
-                }
-            }
-            return null;
-        },
-        amount_paid_with_customer_wallet() {
-            var order = this.pos.get_order();
-            var cashregister = this.find_customer_wallet_payment_method();
-            var total = 0;
-            var lines = order.paymentlines.models;
-            for (var i = 0; i < lines.length; i++) {
-                if (lines[i].cashregister === cashregister) {
-                    total += lines[i].amount;
-                }
-            }
-            return total;
-        },
+
+        /**
+         * Overload function.
+         *
+         * - If wallet journal is selected, check if customer is selected.
+         * - if wallet journal is selected, check if wallet amount is sufficient.
+         */
         order_is_valid: function () {
             if (!this._super()) {
                 return false;
@@ -71,6 +62,13 @@ odoo.define("pos_customer_wallet.screens", function (require) {
 
             return true;
         },
+
+        /**
+         * Overload function.
+         *
+         * Once the order is validated, update the wallet amount
+         * of the current customer, if defined.
+         */
         finalize_validation: function () {
             var wallet_amount = this.amount_paid_with_customer_wallet();
             var client = this.pos.get_client();
@@ -80,6 +78,37 @@ odoo.define("pos_customer_wallet.screens", function (require) {
             }
 
             this._super();
+        },
+
+        /**
+         * Return the payment method of the wallet journal, if exists.
+         *
+         */
+        find_customer_wallet_payment_method() {
+            // This is fairly naive.
+            for (var i = 0; i < this.pos.cashregisters.length; i++) {
+                if (this.pos.cashregisters[i].journal.is_customer_wallet_journal) {
+                    return this.pos.cashregisters[i];
+                }
+            }
+            return null;
+        },
+
+        /**
+         * Return the amount paid with a wallet amount.
+         *
+         */
+        amount_paid_with_customer_wallet() {
+            var order = this.pos.get_order();
+            var cashregister = this.find_customer_wallet_payment_method();
+            var total = 0;
+            var lines = order.paymentlines.models;
+            for (var i = 0; i < lines.length; i++) {
+                if (lines[i].cashregister === cashregister) {
+                    total += lines[i].amount;
+                }
+            }
+            return total;
         },
     });
 });
