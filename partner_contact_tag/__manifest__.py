09e82131a2f7d8e60085222faf91cc3e4cddb673
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-03-09 16:19:34 +0100

    [ADD] partner_contact_tag
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

---
+++ partner_contact_tag/__manifest__.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2023 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Display Tag When Adding Contact to Partner",
+    "summary": """
+        Display tag in the pop-up form view when adding a contact to a partner.""",
+    "version": "16.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://github.com/coopiteasy/addons",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["carmenbianca"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "base",
+    ],
+    "excludes": [],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
---
+++ partner_contact_tag/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Display tag in the pop-up form view when adding a contact to a partner.
