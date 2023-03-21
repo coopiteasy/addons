800d29066c390c618b698e9c83c94994f1998941
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-03-21 17:52:41 +0100

    [REF] Move minimum_wallet_amount field from pos.config to account.journal, cortesy @carmenbianca
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- account_customer_wallet/models/account_journal.py
+++ account_customer_wallet/models/account_journal.py
@@ -10,3 +10,13 @@ class AccountJournal(models.Model):
     is_customer_wallet_journal = fields.Boolean(
         string="Customer Wallet Journal", default=False
     )
+
+    minimum_wallet_amount = fields.Monetary(
+        string="Minimum Wallet Amount",
+        default=0.0,
+        help="Usually 0. You can enter a negative value,"
+        " if you want to accept that the customer wallet"
+        " is negative. Maybe useful if the sale amount"
+        " is slightly higher than the wallet amount,"
+        " to avoid charging the customer a small amount.",
+    )
