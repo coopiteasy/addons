46a39a2907351d9b542269f4092b46a9fd46fc9e
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-04-21 19:13:44 +0200

    [IMP] account_customer_wallet : make the module working with global partners
    **before**
    
    the computation is based on that account  ``customer_wallet_account_id =
    fields.Many2one(related="company_id.customer_wallet_account_id")``
    
    so, if you have a "global" partner, there is no company so no account,
    and the wallet feature will not work.
    
    **After**
    
    The computation is based on ``wallet_account =
    self.env.company.customer_wallet_account_id``. so it depends on the
    context. It will work for global partners, (no company_id) the value
    will depend on the current context. (that makes sense in my opinion). If
    you are in a company A, you have a CoA A and you have a wallet A amount.
    if you are in a company B, you have a coA B and you have a wallet B
    amount.

--- account_customer_wallet/models/res_partner.py
+++ account_customer_wallet/models/res_partner.py
@@ -10,11 +10,6 @@ from odoo.tools.safe_eval import safe_eval
 class Partner(models.Model):
     _inherit = "res.partner"
 
-    customer_wallet_account_id = fields.Many2one(
-        comodel_name="account.account",
-        related="company_id.customer_wallet_account_id",
-        readonly=True,
-    )
     customer_wallet_balance = fields.Monetary(
         compute="_compute_customer_wallet_balance",
         readonly=True,
@@ -35,7 +30,7 @@ class Partner(models.Model):
             .ids
         )
 
-    def get_wallet_balance_account_move_line(self, all_partner_ids, all_account_ids):
+    def get_wallet_balance_account_move_line(self, all_partner_ids, wallet_account_id):
         pre_result = defaultdict(float)
         for line in (
             self.env["account.move.line"]
@@ -48,7 +43,7 @@ class Partner(models.Model):
                     # FIXME: This should ideally be something like
                     # `("account_id", "=", partner.customer_wallet_account_id)`,
                     # but that may not be possible.
-                    ("account_id", "in", list(all_account_ids)),
+                    ("account_id", "=", wallet_account_id.id),
                 ]
             )
         ):
@@ -58,10 +53,12 @@ class Partner(models.Model):
             for partner_id, total in pre_result.items()
         ]
 
-    def get_wallet_balance_all(self, all_partner_ids, all_account_ids):
+    def get_wallet_balance_all(self, all_partner_ids, wallet_account_id):
         # Overload in other modules (like pos_customer_wallet)
         return [
-            self.get_wallet_balance_account_move_line(all_partner_ids, all_account_ids)
+            self.get_wallet_balance_account_move_line(
+                all_partner_ids, wallet_account_id
+            )
         ]
 
     def _compute_customer_wallet_balance(self):
@@ -70,10 +67,12 @@ class Partner(models.Model):
 
         all_partner_families = {}
         all_partner_ids = set()
-        all_account_ids = set()
-
-        for partner in self:
-            all_account_ids.add(partner.customer_wallet_account_id.id)
+        wallet_account_id = self.env.company.customer_wallet_account_id
+        if not wallet_account_id:
+            # No wallet account defined in the current context
+            for partner in self:
+                partner.customer_wallet_balance = 0.0
+            return
 
         # we split the calculation in two part to optimize it
         # because the call of get_all_partners_in_family take time
@@ -86,7 +85,7 @@ class Partner(models.Model):
             all_partner_families[partner] = [partner.id]
             all_partner_ids |= set(all_partner_families[partner])
 
-        all_totals = self.get_wallet_balance_all(all_partner_ids, all_account_ids)
+        all_totals = self.get_wallet_balance_all(all_partner_ids, wallet_account_id)
 
         for partner, child_ids in all_partner_families.items():
             wallet_balance = 0.0
