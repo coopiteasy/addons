ddb3a247340c622ccbec91fbdc8b4a65e9fdece3
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-05-10 16:53:02 +0200

    [ADD] pos_solidarity_rounding_portal
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

---
+++ pos_solidarity_rounding_portal/__manifest__.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2023 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Point of Sale Solidarity Rounding Portal",
+    "summary": """
+        Allow registered users to change their solidarity rounding configuration in the
+        account settings.""",
+    "version": "12.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["carmenbianca"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "pos_solidarity_rounding",
+        "portal",
+    ],
+    "excludes": [],
+    "data": [
+        "views/portal_templates.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
