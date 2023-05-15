8ef7d95f0201f23582f3d31b0abdecaf248167cf
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-05-15 16:17:43 +0200

    [IMP] pos_solidarity_rounding: Only change tip if one doesn't already exist
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- pos_solidarity_rounding/static/src/js/models.js
+++ pos_solidarity_rounding/static/src/js/models.js
@@ -11,6 +11,21 @@ odoo.define("pos_solidarity_rounding.models", function (require) {
             var target_amount = Math.ceil(amount);
             return target_amount - amount;
         },
+        tip_exists: function () {
+            var lines = this.get_orderlines();
+            var tip_product = this.pos.db.get_product_by_id(
+                this.pos.config.tip_product_id[0]
+            );
+
+            if (tip_product) {
+                for (var i = 0; i < lines.length; i++) {
+                    if (lines[i].get_product() === tip_product) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        },
     });
     return models;
 });
