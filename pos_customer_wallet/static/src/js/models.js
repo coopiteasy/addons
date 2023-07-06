a4649a37bab56b484f7627aa9ffb269b6ca3d27c
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-07-06 10:46:41 +0200

    [MIG] pos_customer_wallet: Migration to 16.0

--- pos_customer_wallet/static/src/js/models.js
+++ pos_customer_wallet/static/src/js/models.js
@@ -1,21 +1,19 @@
 odoo.define("pos_customer_wallet.models", function (require) {
     "use strict";
 
-    var models = require("point_of_sale.models");
+    const {Order} = require("point_of_sale.models");
+    const Registries = require("point_of_sale.Registries");
 
-    models.load_fields("res.partner", ["customer_wallet_balance"]);
-    models.load_fields("account.journal", ["is_customer_wallet_journal"]);
-    models.load_fields("product.product", ["is_customer_wallet_product"]);
+    const WalletOrder = (Order_) =>
+        class extends Order_ {
+            export_for_printing() {
+                var json = super.export_for_printing(...arguments);
+                json.customer_wallet_balance = this.partner
+                    ? this.partner.customer_wallet_balance
+                    : 0;
+                return json;
+            }
+        };
 
-    var order_prototype = models.Order.prototype;
-    models.Order = models.Order.extend({
-        export_for_printing: function () {
-            var receipt = order_prototype.export_for_printing.apply(this);
-            var client = this.get("client");
-            receipt.customer_wallet_balance = client
-                ? client.customer_wallet_balance
-                : 0;
-            return receipt;
-        },
-    });
+    Registries.Model.extend(Order, WalletOrder);
 });
