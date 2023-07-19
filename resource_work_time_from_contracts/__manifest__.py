bdbce4b8d3a8441b6d04a6244b0cbded4557b239
Victor Champonnois <victor@coopiteasy.be>
2023-07-19 17:07:09 +0200

    [MIG] resource_work_time_from_contracts: Migration to 16.0

--- resource_work_time_from_contracts/__manifest__.py
+++ resource_work_time_from_contracts/__manifest__.py
@@ -7,7 +7,7 @@
         "Take the contracts of an employee into account when computing work "
         "time per day"
     ),
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Coop IT Easy SC",
     "website": "https://github.com/coopiteasy/addons",
