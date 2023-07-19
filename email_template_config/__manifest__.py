73467833ab4258398f0a1e42aff79b2035fe7a67
Victor Champonnois <victor@coopiteasy.be>
2023-07-19 14:29:03 +0200

    [MIG] email_template_config: Migration to 15.0

--- email_template_config/__manifest__.py
+++ email_template_config/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Email Configuration",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "depends": ["mail"],
     "author": "Coop IT Easy SC",
     "category": "Discuss",
