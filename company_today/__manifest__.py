2ea6b836959d5a85d2282d9478b909dfc43d5bce
Victor Champonnois <victor@coopiteasy.be>
2023-07-26 10:12:54 +0200

    [MIG] company_today: Migration to 13.0

--- company_today/__manifest__.py
+++ company_today/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Company Today",
     "summary": """
         Store today's date on the company model.""",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/coopiteasy/addons",
     "author": "Coop IT Easy SC",
