15714fb731f713608475c70928f0b98e0137e6ac
Victor Champonnois <victor@coopiteasy.be>
2023-08-08 15:13:41 +0200

    [MIG] sale_order_volume: Migration to 14.0

--- sale_order_volume/__manifest__.py
+++ sale_order_volume/__manifest__.py
@@ -18,7 +18,7 @@
 ##############################################################################
 {
     "name": "Sale Order Volume",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": ["sale", "stock", "website_sale"],
     "author": "Coop IT Easy SC",
     "license": "AGPL-3",
