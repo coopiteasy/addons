b4c8c4c92b6786fa2dd41935d989fcee48bb9a06
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-09-08 15:00:53 +0200

    [FIX] pos_customer_wallet: Add some copyright headers
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- pos_customer_wallet/static/src/js/Screens/PaymentScreen/PaymentScreen.js
+++ pos_customer_wallet/static/src/js/Screens/PaymentScreen/PaymentScreen.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Coop IT Easy SC
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 odoo.define("pos_customer_wallet.PaymentScreen", function (require) {
     "use strict";
     const PaymentScreen = require("point_of_sale.PaymentScreen");
--- pos_customer_wallet/static/src/js/models.js
+++ pos_customer_wallet/static/src/js/models.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Coop IT Easy SC
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 odoo.define("pos_customer_wallet.models", function (require) {
     "use strict";
 
