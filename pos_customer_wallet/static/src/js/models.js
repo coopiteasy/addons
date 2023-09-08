a27cd3c12a040be1cf1202224cea6447bd258598
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-09-08 14:52:10 +0200

    [FIX] pos_customer_wallet: Run pre-commit + satisfy eslint
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- pos_customer_wallet/static/src/js/models.js
+++ pos_customer_wallet/static/src/js/models.js
@@ -1,19 +1,19 @@
 odoo.define("pos_customer_wallet.models", function (require) {
     "use strict";
 
-    const { Order } = require('point_of_sale.models');
-    const Registries = require('point_of_sale.Registries');
+    const {Order} = require("point_of_sale.models");
+    const Registries = require("point_of_sale.Registries");
 
-
-    const WalletOrder = (Order) => class WalletOrder extends Order {
-        export_for_printing() {
-            var json = super.export_for_printing(...arguments);
-            json.customer_wallet_balance = this.partner
-                ? this.partner.customer_wallet_balance
-                : 0;
-            return json;
-        }
-    };
+    const WalletOrder = (Order_) =>
+        class extends Order_ {
+            export_for_printing() {
+                var json = super.export_for_printing(...arguments);
+                json.customer_wallet_balance = this.partner
+                    ? this.partner.customer_wallet_balance
+                    : 0;
+                return json;
+            }
+        };
 
     Registries.Model.extend(Order, WalletOrder);
 });
