6848da51e1ee8d8398d12b7b16f6644d65dc06bf
Victor Champonnois <victor@coopiteasy.be>
2024-01-25 17:15:03 +0100

    [MIG] document_hosting: Migration to 15.0

--- document_hosting/__manifest__.py
+++ document_hosting/__manifest__.py
@@ -10,7 +10,7 @@
     """,
     "author": "Coop IT Easy SC",
     "license": "AGPL-3",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "website": "https://github.com/coopiteasy/addons",
     "category": "Document",
     "depends": ["base", "web", "website", "mail"],
