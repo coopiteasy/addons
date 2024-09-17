a2efbcc3c70a04e09255b3157cb6b45fb273a60e
Rémy Taymans <remytms@tsmail.eu>
2024-09-17 17:30:31 +0200

    [IMP] e_t_speaker_catering: remove default add constrains

--- event_track_speaker_catering/models/event_track_speaker_meal_date.py
+++ event_track_speaker_catering/models/event_track_speaker_meal_date.py
@@ -1,10 +1,8 @@
-# Copyright 2017 David Vidal<david.vidal@tecnativa.com>
-# Copyright 2017 Tecnativa - Pedro M. Baeza
+# Copyright 2024 Coop IT Easy <https://coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from datetime import datetime
-
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class EventTrackSpeakerMealDate(models.Model):
@@ -15,9 +13,15 @@ class EventTrackSpeakerMealDate(models.Model):
     event_id = fields.Many2one(
         "event.event", related="speaker_id.event_id", string="Event", store="True"
     )
+    date = fields.Date()
 
-    @api.depends("event_id.date_begin")
-    def get_event_begin_date(self):
-        return self.event_id.date_begin
-
-    date = fields.Date(default=datetime.strftime("2020-01-01"))
+    @api.constrains("date")
+    def _constrain_date_valid(self):
+        for meal_date in self:
+            if (
+                meal_date.date < meal_date.event_id.date_begin.date()
+                or meal_date.date > meal_date.event_id.date_end.date()
+            ):
+                raise ValidationError(
+                    "Meal date must match the event dates of the speaker."
+                )
