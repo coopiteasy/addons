6add12a213cb41a41640e0c3fb121938c2d1c2da
Rémy Taymans <remytms@tsmail.eu>
2024-09-19 00:28:03 +0200

    [ADD] e_t_multi_date: split date and time

--- event_track_multi_date/models/event_track_date.py
+++ event_track_multi_date/models/event_track_date.py
@@ -3,7 +3,9 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 
-from odoo import fields, models
+from datetime import datetime
+
+from odoo import api, fields, models
 
 
 class EventTrackDate(models.Model):
@@ -11,4 +13,24 @@ class EventTrackDate(models.Model):
     _description = "Track dates"
 
     track_id = fields.Many2one("event.track")
-    date = fields.Datetime()
+    datetime = fields.Datetime(string="Date and time")
+    date = fields.Date(compute="_compute_date_and_time")
+    hour = fields.Float(string="Hours", compute="_compute_date_and_time")
+
+    @api.depends("date")
+    def _compute_date_and_time(self):
+        for track_date in self:
+            if track_date.datetime:
+                track_date.date = track_date.datetime.date()
+                track_date.hour = (
+                    fields.Datetime.context_timestamp(
+                        self.env.user, track_date.datetime
+                    )
+                    - fields.Datetime.context_timestamp(
+                        self.env.user,
+                        datetime.combine(track_date.date, datetime.min.time()),
+                    ).replace(hour=0, minute=0, second=0)
+                ).total_seconds() / 3600
+            else:
+                track_date.date = False
+                track_date.hour = False
