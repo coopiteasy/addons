924d02960e55932eef15f1e0bfee784db0f46380
Rémy Taymans <remytms@tsmail.eu>
2024-09-18 23:28:51 +0200

    [FIX] e_t_multi_date: wrong copyright and imp security

--- event_track_multi_date/__manifest__.py
+++ event_track_multi_date/__manifest__.py
@@ -1,5 +1,6 @@
-# Copyright 2017-19 David Vidal<david.vidal@tecnativa.com>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# SPDX-FileCopyrightText: 2024 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 {
     "name": "Multiple Dates per Track",
@@ -7,7 +8,7 @@
     "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/coopiteasy/addons",
-    "category": "Marketing",
+    "category": "Event",
     "summary": "Multiple Dates per Track",
     "depends": ["website_event_track"],
     "data": [
