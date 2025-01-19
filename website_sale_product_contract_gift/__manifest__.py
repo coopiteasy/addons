96eeae12578dd1e99c8d8ac0ea3b5ca2d0008ea4
Rémy Taymans <remytms@tsmail.eu>
2025-01-19 20:10:12 +0100

    [ADD] website_sale_product_contract_gift
    Add product contract that can be set as gift and therefore generate some
    specific compatibilities and flow.

---
+++ website_sale_product_contract_gift/__manifest__.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2024 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Website Sale Product Contract Gift",
+    "summary": """
+        Configure product contract to be a gift to someone else.""",
+    "version": "16.0.1.0.0",
+    "category": "Website",
+    "website": "https://github.com/coopiteasy/addons",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["remytms"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "website_sale_product_compatibility",
+        "product_contract",
+    ],
+    "excludes": [],
+    "data": [
+        "views/product_views.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
---
+++ website_sale_product_contract_gift/models/sale_order.py
@@ -0,0 +1,48 @@
+# SPDX-FileCopyrightText: 2024 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.constrains("order_line")
+    def _check_gift_alone(self):
+        """Ensure gift are not mixed in a sale order."""
+        for order in self:
+            at_least_one_gift = any(
+                order.order_line.mapped("product_id").mapped("is_gift")
+            )
+            is_all_gift = all(order.order_line.mapped("product_id").mapped("is_gift"))
+            if at_least_one_gift and not is_all_gift:
+                raise ValidationError(
+                    _(
+                        "Cannot add product gift in an order that "
+                        "contains other product that are not gifts."
+                    )
+                )
+
+    def check_product_compatibility(self, product_id):
+        warning = super().check_product_compatibility(product_id)
+        if not warning:
+            product = self.env["product.product"].browse(product_id).exists()
+            non_gifts = self.order_line.mapped("product_id").filtered(
+                lambda p: not p.is_gift
+            )
+            is_all_gift = all(self.order_line.mapped("product_id").mapped("is_gift"))
+            if product:
+                if product.is_gift and non_gifts:
+                    warning = _(
+                        f"Product {product.name} cannot be added because "
+                        "it's a gift and gift must be purchase seperatly."
+                    )
+                elif not product.is_gift and is_all_gift:
+                    warning = _(
+                        f"Product {product.name} cannot be added because "
+                        "it is not a gift and other product are gifts."
+                    )
+        return warning
