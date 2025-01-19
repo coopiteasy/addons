6e8ff34214b47055a89a7293f4db86d397c6995b
Rémy Taymans <remytms@tsmail.eu>
2025-01-19 20:02:24 +0100

    [ADD] website_sale_product_compatibility
    This module is a base module for adding compatibility check between
    product in a sale order for the e-commerce.
    
    This comes with the refactoring of the module
    website_sale_restrict_sepa_dd.

--- website_sale_product_compatibility/__manifest__.py
+++ website_sale_product_compatibility/__manifest__.py
@@ -3,23 +3,22 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 {
-    "name": "Restrict SEPA Direct Debit Payment",
+    "name": "Website Sale Product Compatibility",
     "summary": """
-        Restrict payment by SEPA Direct Debit for some products.""",
+        Generic module to add compatibility check between products.""",
     "version": "16.0.1.0.0",
-    "category": "Website",
+    "category": "E-Commerce",
     "website": "https://github.com/coopiteasy/addons",
     "author": "Coop IT Easy SC",
     "maintainers": ["remytms"],
     "license": "AGPL-3",
     "application": False,
     "depends": [
-        "payment_sepa_dd",
-        "website_sale_product_compatibility",
+        "website_sale",
     ],
     "excludes": [],
     "data": [
-        "views/product_views.xml",
+        "views/templates.xml",
     ],
     "demo": [],
     "qweb": [],
---
+++ website_sale_product_compatibility/models/sale_order.py
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: 2024 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def check_product_compatibility(self, product_id):
+        """Override this function to detect incompatibility between
+        products.
+
+        Warning: self.ensure_one()
+
+        If an empty string is returned then the product_id is compatible
+        and will be added to the sale order. If a warning string is
+        returned then the product is considered not compatible with the
+        order and the product will not be added to the order.
+
+        By default this method always return no warning message.
+
+        :product_id: The id of the product to check compatibility.
+        :rtype: str
+        :retrun: warning message to be shown on the web interface.
+        """
+        self.ensure_one()
+        return ""
+
+    def _cart_update(self, product_id, line_id=None, add_qty=0, set_qty=0, **kwargs):
+        """Prevent incompatible product to be added to the cart."""
+        self.ensure_one()
+        warning = self.check_product_compatibility(product_id)
+        if warning:
+            add_qty = None
+            set_qty = 0
+        values = super()._cart_update(
+            product_id=product_id,
+            line_id=line_id,
+            add_qty=add_qty,
+            set_qty=set_qty,
+            **kwargs,
+        )
+        values["warning"] = warning
+        return values
