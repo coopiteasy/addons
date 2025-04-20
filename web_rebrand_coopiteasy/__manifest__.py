5e8195c5a345d7f9a4cc0f1aa4c2a81e796a4525
hugues de keyzer <odoo@hugues.info>
2025-04-20 23:07:37 +0200

    [REF] refactor website_rebrand_coopiteasy
    move coop it easy rebranding to a new web_rebrand_coopiteasy module
    which only depends on web and also rebrands the default login page (used
    when website is not installed) and make website_rebrand_coopiteasy
    depend on it.

---
+++ web_rebrand_coopiteasy/__manifest__.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2025 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Coop IT Easy Branding",
+    "summary": "Replace default Odoo branding by Coop IT Easy branding",
+    "version": "16.0.1.0.0",
+    "category": "Hidden",
+    "website": "https://github.com/coopiteasy/addons",
+    "author": "Coop IT Easy SC",
+    "license": "AGPL-3",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "templates/rebrand_coopiteasy.xml",
+    ],
+}
