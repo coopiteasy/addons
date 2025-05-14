5d7364825b3fbc51ec2f2387780fcf7087a0f20f
Simon Hick <sim@coopiteasy.be>
2025-05-14 11:00:19 +0200

    [ADD] board_signature: adds name, function and signature of board representative to companies

---
+++ board_signature/__manifest__.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2025 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Board signature",
+    "summary": "Adds board representative's name, function and signature to companies",
+    "version": "16.0.1.0.0",
+    "category": "Cooperative management",
+    "website": "https://github.com/coopiteasy/addons",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["mihien"],
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["views/res_company_view.xml"],
+}
