d2f5f89d3c5356a2466fe94dd309363dcaeb0221
Simon Hick <sim@coopiteasy.be>
2025-07-28 16:36:18 +0200

    [MIG] product_label_report: Migration to 16.0

--- product_label_report/__manifest__.py
+++ product_label_report/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Coop IT Easy SC",
     "category": "Product",
-    "version": "12.0.1.0.0",
+    "version": "16.0.1.0.0",
     "website": "https://github.com/coopiteasy/addons",
     "license": "AGPL-3",
     "depends": ["base", "product", "web"],
