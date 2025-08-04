3fa1516223c9e90dcb5d28557dcc1b0b00e4457e
Simon Hick <sim@coopiteasy.be>
2025-08-04 14:30:37 +0200

    [REN] Rename module to purchase_quick_usability
    Rename demain_purchase_quick to purchase_quick_usability.

--- purchase_quick_usability/__manifest__.py
+++ purchase_quick_usability/__manifest__.py
@@ -3,12 +3,12 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 {
-    "name": "Demain Purchase Quick",
+    "name": "Purchase Quick Usability",
     "summary": "Add minimum quantity and purchase UoM fields to purchase "
     "order products quick add view",
     "version": "16.0.1.0.0",
     "category": "Purchase",
-    "website": "https://coopiteasy.be",
+    "website": "https://github.com/coopiteasy/addons",
     "author": "Coop IT Easy SC",
     "license": "AGPL-3",
     "application": False,
