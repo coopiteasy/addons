adfd044e776e527cdc4f7bc0c34a1b9a3109772f
Simon Hick <sim@coopiteasy.be>
2025-08-07 23:22:25 +0200

    [IMP] pos_solidarity_rounding: pre-commit stuff

--- pos_solidarity_rounding/__manifest__.py
+++ pos_solidarity_rounding/__manifest__.py
@@ -8,7 +8,7 @@
         Round POS payments up for willing customers as a gesture of solidarity.""",
     "version": "12.0.1.0.0",
     "category": "Point of Sale",
-    "website": "https://coopiteasy.be",
+    "website": "https://github.com/coopiteasy/addons",
     "author": "Coop IT Easy SC",
     "maintainers": ["carmenbianca"],
     "license": "AGPL-3",
