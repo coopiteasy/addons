8004cc5aae6f78faa99c15e092d976ebf89fbeca
Victor Champonnois <victor@coopiteasy.be>
2025-08-27 15:18:16 +0200

    [FIX] _compute_qty_to_invoice on SO created from contract

--- product_contract_sale_generation/models/sale_order_line.py
+++ product_contract_sale_generation/models/sale_order_line.py
@@ -4,16 +4,23 @@
 
 from odoo import models
 
+from odoo.addons.sale.models.sale_order_line import SaleOrderLine as SaleSaleOrderLine
 
-class SaleOrder(models.Model):
+
+class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     def _compute_qty_to_invoice(self):
         res = super()._compute_qty_to_invoice()
+        # The product_contract module overrides the _compute_qty_to_invoice()
+        # method and sets qty_to_invoice to 0 if the associated product is
+        # "is_contract". This is needed because the SO that will create the
+        # contract should not be invoiced. However, the
+        # contract_sale_generation module allows to produce new SOs, with the
+        # same products, from the contract. These SOs need to be invoiced.
+        # Therefore for these SOs "created from contract", the qty_to_invoice
+        # should not be set to 0, but be computed by the standard odoo method.
         for line in self:
             if line.order_id.created_from_contract:
-                # put back the right quantity to invoice
-                line.filtered("product_id.is_contract").update(
-                    {"qty_to_invoice": line.product_uom_qty}
-                )
+                SaleSaleOrderLine._compute_qty_to_invoice(line)
         return res
