4717a62b6a85ed78a8bca6f5a4f04207c6f00de8
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-03-12 12:37:53 +0100

    [FIX] customer_wallet: typo in filename

--- customer_wallet/__manifest__.py
+++ customer_wallet/__manifest__.py
@@ -25,7 +25,7 @@
         "views/res_config_settings_views.xml",
         "wizards/account_payment_register_views.xml",
         "wizards/customer_wallet_detail_wizard_views.xml",
-        "wizards/customer_wallet_reditribute_views.xml",
+        "wizards/customer_wallet_redistribute_views.xml",
     ],
     "demo": [
         "demo/account_account_demo.xml",
